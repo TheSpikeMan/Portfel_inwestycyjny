f85763968113fc4ab2ac7f63d1174a8c2e47ca72
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-01-13 13:06:00 +0100

    Dostosowano kod pod obiekt Client, zmieniono aktywator na Pub/Sub. Wersja działająca

--- oprocentowanie_obligacji_skarbowych.py
+++ oprocentowanie_obligacji_skarbowych.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  8 16:56:15 2024
-
-@author: grzeg
-"""
-
+import base64
+import functions_framework
+import pandas as pd
+import pandas_gbq
 import requests
 from bs4 import BeautifulSoup
+from google.cloud import bigquery
 import re
 
-# 0. Import biblioteki do wykomentowania w Cloud Functions
-# from google.cloud import bigquery
-import pandas as pd
-import functions_framework
-
 @functions_framework.cloud_event
-def Treasury_Bonds(cloud_event):
+def Treasury_bonds(cloud_event):
     # 1. Zdefiniowanie parametrów tabeli, przechowującej danej transakcyjne.
     project_id = 'projekt-inwestycyjny'
     dataset_id = 'Transactions'
     table_id = 'Transactions_view'
     destination_table = f"`{project_id}.{dataset_id}.{table_id}`"
+
+    # 1.5. Zdefiniowanie parametrów tabeli, do której pisze CF.
+    project_id_2 = 'projekt-inwestycyjny'
+    dataset_id_2 = 'Dane_instrumentow'
+    table_id_2 = 'Treasury_Bonds'
+    destination_table_2 = f"{project_id_2}.{dataset_id_2}.{table_id_2}"
     
     # 2. Zdefiniowanie kwerendy wyciągającej wyłącznie dane transakcyjne obligacji.
     query = f"""
@@ -41,7 +40,7 @@ def Treasury_Bonds(cloud_event):
     # transakcje.
     ticker_list = list(set(dane_transakcyjne['Ticker'].to_list()))
     tablica_oprocentowania = pd.DataFrame()
-    
+
     # 5. Dla każdego instrumentu obligacji skarbowych.
     for ticker in ticker_list:
         ticker_obligacji = ticker
@@ -51,45 +50,44 @@ def Treasury_Bonds(cloud_event):
             website = "https://www.obligacjeskarbowe.pl/oferta-obligacji/obligacje-10-letnie-edo/" +\
                 ticker_obligacji + "/"
                 
-            r = requests.get(website)
-            
+            with requests.get(website) as r:
             # 7. Jeżeli udało się podłączyć do strony.
-            if r.status_code == 200:
+
+                if r.status_code == 200:
+                    raw_data = r.text
+                    soup = BeautifulSoup(raw_data, 'html.parser')
+                    
+                    # 8. Schemat to obiekt, który zczyna się od znaku dziesiętnego,
+                    # następnie zawiera przecinek, dwa znaki dziesiętne, dokładnie
+                    # jeden znak '%', a następnie dowolną liczba dowolnych znaków.
+                    pattern = re.compile(r'\d,\d{2}%{1}.*')
                 
-                raw_data = r.text
-                soup = BeautifulSoup(raw_data, 'html.parser')
-                
-                # 8. Schemat to obiekt, który zczyna się od znaku dziesiętnego,
-                # następnie zawiera przecinek, dwa znaki dziesiętne, dokładnie
-                # jeden znak '%', a następnie dowolną liczba dowolnych znaków.
-                pattern = re.compile(r'\d,\d{2}%{1}.*')
-            
-                # 9. Przeszukiwane konkretnego elementu strony.
-                result_set = soup.find_all('span', class_='product-details__list-value')
-                raw_string = result_set[1].text
-                
-                # 10. Rozdzielenie całego tekstu do pojedynczych słów/
-                string_modified = raw_string.split()
-                for word in string_modified:
-                    # 11. Jeżeli znajdziesz słowo, które spełnia pattern
-                    if re.search(pattern, word):
-                        oprocentowanie_zmienne = (word[:4].replace(",", "."))
-                        oprocentowanie_zmienne = float(oprocentowanie_zmienne)
-            else:
-                continue
-        
-            # 12. Szukanie oprocentowania obligacji w pierwszym roku.
-            # Wykorzystanie w tym celu obiektu o zadanych niżej parametrach.
-            result_set_2= soup.find_all('figure', class_ = 'hero__image')
-            oprocentowanie_pierwszy_rok = result_set_2[0].text.strip()[:4].\
-                replace(",", ".")
-            oprocentowanie_pierwszy_rok = float(oprocentowanie_pierwszy_rok)
-            
-            # 13. Przypisanie wszystkich elementów do listy.
-            tablica_oprocentowania_tickera = [ticker_obligacji,
-                                              oprocentowanie_pierwszy_rok,
-                                              oprocentowanie_zmienne]
-            
+                    # 9. Przeszukiwane konkretnego elementu strony.
+                    result_set = soup.find_all('span', class_='product-details__list-value')
+                    raw_string = result_set[1].text
+                    
+                    # 10. Rozdzielenie całego tekstu do pojedynczych słów/
+                    string_modified = raw_string.split()
+                    for word in string_modified:
+                        # 11. Jeżeli znajdziesz słowo, które spełnia pattern
+                        if re.search(pattern, word):
+                            oprocentowanie_zmienne = (word[:4].replace(",", "."))
+                            oprocentowanie_zmienne = float(oprocentowanie_zmienne)
+                    
+                    # 12. Szukanie oprocentowania obligacji w pierwszym roku.
+                    # Wykorzystanie w tym celu obiektu o zadanych niżej parametrach.
+                    result_set_2= soup.find_all('figure', class_ = 'hero__image')
+                    oprocentowanie_pierwszy_rok = result_set_2[0].text.strip()[:4].\
+                        replace(",", ".")
+                    oprocentowanie_pierwszy_rok = float(oprocentowanie_pierwszy_rok)
+
+                    # 13. Przypisanie wszystkich elementów do listy.
+                    tablica_oprocentowania_tickera = [ticker_obligacji,
+                    oprocentowanie_pierwszy_rok,
+                    oprocentowanie_zmienne]
+                else:
+                    pass
+
             # 14. Połączenie obecnych danych w DataFrame (pierwszy element) z
             # dokładanymi elementami (pd.DataFrame(tablica_(...)))
             tablica_oprocentowania = pd.concat([tablica_oprocentowania, 
@@ -104,12 +102,6 @@ def Treasury_Bonds(cloud_event):
     tablica_oprocentowania.reset_index(drop=True, inplace=True)
     
     
-    # 16. Zdefiniowanie danych parametryzacyjnych, do celów wysłania do BigQuery.
-    project_id = 'projekt-inwestycyjny'
-    dataset_id = 'Dane_instrumentow'
-    table_id = 'Treasury_Bonds'
-    destination_table = f"{project_id}.{dataset_id}.{table_id}"
-    
     # 17. Zdefiniowanie schematu danych w BQ
     schema = [bigquery.SchemaField(name = 'Ticker', field_type = "STRING", \
                                    mode = "REQUIRED"),
@@ -117,41 +109,50 @@ def Treasury_Bonds(cloud_event):
                                    mode = "REQUIRED"),
               bigquery.SchemaField(name = 'Regular_interest', field_type = "FLOAT",\
                                    mode = "REQUIRED")]
+
     
     # 18. Wyznaczenie konfiguracji dla joba i wykonanie joba.
     job_config = bigquery.LoadJobConfig(schema = schema,
                                         write_disposition = "WRITE_TRUNCATE")
     try:
         job = client.load_table_from_dataframe(tablica_oprocentowania, 
-                                               destination_table,
+                                               destination_table_2,
                                                job_config = job_config)
         job.result()
     except Exception as e:
         print(f"Error uploading data to BigQuery: {str(e)}")
+        return "Błąd eksportu danych do BigQuery."
     
     print("Dane obligacji skarbowych zostały przekazane do tabeli BigQuery.")
+    return "Program zakończył się pomyślnie."
     
 """
 Konfiguracja:
     
 Nazwa funkcji: Treasury_bonds
 Region: europe-central2 (Warszawa)
-Pamięć przydzielona: 256 MiB
-CPU: 0.167
+Pamięć przydzielona: 512 MiB
+CPU: 0.333
 Przekroczony limit czasu: 60
 Maksymalna liczba żądań równoczesnych na instancję: 1
 Minimalna liczba instancji: 0 
-Maksymalna liczba instancji: 100
+Maksymalna liczba instancji: 1
 Konto usługi: Default compute service account 
     
 Aktywator Eventarc:
     - Typ aktywatora: Źródła Google
-    - Dostawca zdarzeń: Big Query
-    - Zdarzenie: google.cloud.bigquery.v2.JobService.Insert.Job
-    - Zasób: Okreslony zasób
-    - Pełna nazwa zasobu: projects/projekt-inwestycyjny/datasets/Dane_instrumentow/tables/Instruments
+    - Dostawca zdarzeń: Cloud Pub/Sub
+    - Zdarzenie: google.cloud.pubsub.topic.v1.messagePublished
     - Region: europe-central2 (Warszawa)
-    - Konto usługi: Default compute service account
+    - Konto usługi: compute@developer.gserviceaccount.com 
     
+Requirements:
+    
+functions-framework==3.*
+bs4
+requests
+pandas
+google.cloud
+pandas_gbq
     
 """
