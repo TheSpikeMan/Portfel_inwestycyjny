218c3c05de68c788750a6a8e1fec434d30d24116
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-01-13 21:10:07 +0100

    Dodano mechanizm wysyłający do Big Query dane. Do sprawdzenia

--- obligacje_skarbowe.py
+++ obligacje_skarbowe.py
@@ -10,147 +10,210 @@ from datetime import date
 from datetime import timedelta
 import math
 from google.cloud import bigquery
- 
-project_id = 'projekt-inwestycyjny'
-dataset_id_1 = 'Inflation'
-table_id_1 = 'Inflation'
-destination_table_1 = f"`{project_id}.{dataset_id_1}.{table_id_1}`"
+import base64
+import functions_framework
+import pandas_gbq
+from flask import Flask, request
 
-dataset_id_2 = 'Transactions'
-table_id_2 = 'Transactions_view'
-destination_table_2 = f"`{project_id}.{dataset_id_2}.{table_id_2}`"
-
-dataset_id_3 = 'Dane_instrumentow'
-table_id_3 = 'Treasury_Bonds'
-destination_table_3 = f"`{project_id}.{dataset_id_3}.{table_id_3}`"
-
-query_1 = f"""
-    SELECT
-        inflation,
-        date
-    FROM {destination_table_1}
-    WHERE TRUE
-    """
+@functions_framework.cloud_event
+def Treasury_bonds_daily(cloud_event):
+     
+    project_id = 'projekt-inwestycyjny'
+    dataset_id_1 = 'Inflation'
+    table_id_1 = 'Inflation'
+    destination_table_1 = f"`{project_id}.{dataset_id_1}.{table_id_1}`"
     
-query_2 = f"""
-    SELECT
-        *
-    FROM  {destination_table_2}
-    WHERE
-        Instrument_type_id = 5
-    """
+    dataset_id_2 = 'Transactions'
+    table_id_2 = 'Transactions_view'
+    destination_table_2 = f"`{project_id}.{dataset_id_2}.{table_id_2}`"
     
-query_3 = f"""
-    SELECT
-        *
-    FROM  {destination_table_3}
-    WHERE
-        TRUE
-    """
-
-client = bigquery.Client()
-query_job_1 = client.query(query_1)
-query_job_2 = client.query(query_2)
-query_job_3 = client.query(query_3)
-dane_inflacyjne = query_job_1.to_dataframe()
-dane_transakcyjne = query_job_2.to_dataframe()
-dane_marz = query_job_3.to_dataframe()
-
-dane_inflacyjne.columns = ['Inflacja', 'Początek miesiąca']
-dane_inflacyjne['Inflacja'] = dane_inflacyjne['Inflacja'].astype('string')
-dane_inflacyjne['Inflacja'] = dane_inflacyjne['Inflacja'].str.replace(".", ",")
-
-
-dane_obligacji = dane_transakcyjne.merge(right=dane_marz, 
-                                 how='inner', 
-                                 on = 'Ticker')
-
+    dataset_id_3 = 'Dane_instrumentow'
+    table_id_3 = 'Treasury_Bonds'
+    destination_table_3 = f"`{project_id}.{dataset_id_3}.{table_id_3}`"
     
-    
-ticker_list = (dane_obligacji['Ticker'])
-dates_list = (dane_obligacji['Transaction_date'])
-wolumen_list = (dane_obligacji['Transaction_amount'])
-marza_pierwszy_rok_list = (dane_obligacji['First_year_interest'])
-marza_kolejne_lata_list = (dane_obligacji['Regular_interest'])
-
-dane_do_analizy = pd.concat([ticker_list, 
-                   dates_list, 
-                   wolumen_list,
-                   marza_pierwszy_rok_list,
-                   marza_kolejne_lata_list], 
-                   axis=1)
-
-
-# analityka - szukanie wartosci aktualnej
-result_df = pd.DataFrame(columns=['Ticker', 'Date', 'Current Value'])
-
-for dane in dane_do_analizy.iterrows():
-    ticker = dane[1][0]
-    data_zakupu = dane[1][1]
-    wolumen = dane[1][2]
-    marza_pierwszy_rok = dane[1][3]
-    marza_kolejne_lata = dane[1][4]
-    
-
-    # calculating the start_value
-    wolumen_jednostkowy = 100
-    start_value = wolumen * wolumen_jednostkowy
-    
-    # calculating current date, finding the end date, calculating the number of days and years
-    current_date = date.today()
-    liczba_dni = (current_date - data_zakupu).days
-    liczba_lat = int(math.modf(liczba_dni/365)[1])
-
+    query_1 = f"""
+        SELECT
+            inflation,
+            date
+        FROM {destination_table_1}
+        WHERE TRUE
+        """
         
-    # writing the code to find current value
-    n = 1
-    if liczba_dni < 365:
-        current_value = start_value + start_value * liczba_dni / 365 * (marza_pierwszy_rok/100)
+    query_2 = f"""
+        SELECT
+            *
+        FROM  {destination_table_2}
+        WHERE
+            Instrument_type_id = 5
+        """
+        
+    query_3 = f"""
+        SELECT
+            *
+        FROM  {destination_table_3}
+        WHERE
+            TRUE
+        """
     
-    else:
-        current_value = start_value + start_value * (marza_pierwszy_rok/100)
-        print(f"Obligacje zostały zakupione w dniu {data_zakupu} na wolumen {wolumen} sztuk.")
-        print(f"Marża w pierwszym roku wyniosła {marza_pierwszy_rok}%.")
-        print(f"Wartosc inwestycji, po pierwszym roku to {current_value} zł.\n")
-        for i in range(liczba_lat, 0, -1):
-            liczba_dni_przesuniecie = timedelta(days= 365 * n - 60)
-            data_badania_inflacji = date((data_zakupu + liczba_dni_przesuniecie).year, \
-                                 (data_zakupu + liczba_dni_przesuniecie).month, \
-                                 1)
-            inflacja = dane_inflacyjne.loc[dane_inflacyjne['Początek miesiąca'] == str(data_badania_inflacji)]\
-                .iloc[:,:1]
-            inflacja['Inflacja'] = inflacja['Inflacja'].str.replace(',','.')
-            inflacja = float(inflacja.iloc[0,0])
-            print(f"{n}. Inflacja została zbadana dnia {data_badania_inflacji} i wynosi {inflacja}%.")
-            if liczba_dni < 730:
-                current_value = current_value + current_value * \
-                    (liczba_dni - 365)/365 * (inflacja + marza_kolejne_lata)/ 100
-                print(f"Oprocentowanie w tym okresie wynosi {inflacja + marza_kolejne_lata}%.")
-                print(f"Wartosc po tym okresie wynosi {round(current_value,2)} zł.\n")
-            else:
-                current_value = current_value + current_value * \
-                    (inflacja + marza_kolejne_lata) / 100
-                liczba_dni = liczba_dni - 365
-                print(f"Oprocentowanie w tym okresie wynosi {inflacja + marza_kolejne_lata}%.")
-                print(f"Wartosc po tym okresie wynosi {round(current_value,2)} zł.\n")
-            n = n + 1 
+    client = bigquery.Client()
+    query_job_1 = client.query(query_1)
+    query_job_2 = client.query(query_2)
+    query_job_3 = client.query(query_3)
+    dane_inflacyjne = query_job_1.to_dataframe()
+    dane_transakcyjne = query_job_2.to_dataframe()
+    dane_marz = query_job_3.to_dataframe()
+    
+    dane_inflacyjne.columns = ['Inflacja', 'Początek miesiąca']
+    dane_inflacyjne['Inflacja'] = dane_inflacyjne['Inflacja'].astype('string')
+    dane_inflacyjne['Inflacja'] = dane_inflacyjne['Inflacja'].str.replace(".", ",")
+    
+    
+    dane_obligacji = dane_transakcyjne.merge(right=dane_marz, 
+                                     how='inner', 
+                                     on = 'Ticker')
+    
+        
+        
+    ticker_list = (dane_obligacji['Ticker'])
+    dates_list = (dane_obligacji['Transaction_date'])
+    wolumen_list = (dane_obligacji['Transaction_amount'])
+    marza_pierwszy_rok_list = (dane_obligacji['First_year_interest'])
+    marza_kolejne_lata_list = (dane_obligacji['Regular_interest'])
+    
+    dane_do_analizy = pd.concat([ticker_list, 
+                       dates_list, 
+                       wolumen_list,
+                       marza_pierwszy_rok_list,
+                       marza_kolejne_lata_list], 
+                       axis=1)
+    
+    
+    # analityka - szukanie wartosci aktualnej
+    result_df = pd.DataFrame(columns=['Ticker', 'Date', 'Current Value'])
+    
+    for dane in dane_do_analizy.iterrows():
+        ticker = dane[1][0]
+        data_zakupu = dane[1][1]
+        wolumen = dane[1][2]
+        marza_pierwszy_rok = dane[1][3]
+        marza_kolejne_lata = dane[1][4]
+        
+    
+        # calculating the start_value
+        wolumen_jednostkowy = 100
+        start_value = wolumen * wolumen_jednostkowy
+        
+        # calculating current date, finding the end date, calculating the number of days and years
+        current_date = date.today()
+        liczba_dni = (current_date - data_zakupu).days
+        liczba_lat = int(math.modf(liczba_dni/365)[1])
+    
             
-    print(f"Wartosc w dniu obecnym dla tickera {ticker} wynosi {round(current_value, 2)} zł.")
-    result_df = pd.concat([result_df, \
-                           pd.DataFrame(data=[[ticker, data_zakupu, \
-                                               round(current_value, 2)]], \
-                                        columns=['Ticker', 'Date', 'Current Value'])])
-    result_dfs = result_df.merge(right=dane_obligacji, 
-                    how='inner',
-                    left_on=['Ticker', 'Date'],
-                    right_on= ['Ticker', 'Transaction_date'])
+        # writing the code to find current value
+        n = 1
+        if liczba_dni < 365:
+            current_value = start_value + start_value * liczba_dni / 365 * (marza_pierwszy_rok/100)
+        
+        else:
+            current_value = start_value + start_value * (marza_pierwszy_rok/100)
+            print(f"Obligacje zostały zakupione w dniu {data_zakupu} na wolumen {wolumen} sztuk.")
+            print(f"Marża w pierwszym roku wyniosła {marza_pierwszy_rok}%.")
+            print(f"Wartosc inwestycji, po pierwszym roku to {current_value} zł.\n")
+            for i in range(liczba_lat, 0, -1):
+                liczba_dni_przesuniecie = timedelta(days= 365 * n - 60)
+                data_badania_inflacji = date((data_zakupu + liczba_dni_przesuniecie).year, \
+                                     (data_zakupu + liczba_dni_przesuniecie).month, \
+                                     1)
+                inflacja = dane_inflacyjne.loc[dane_inflacyjne['Początek miesiąca'] == str(data_badania_inflacji)]\
+                    .iloc[:,:1]
+                inflacja['Inflacja'] = inflacja['Inflacja'].str.replace(',','.')
+                inflacja = float(inflacja.iloc[0,0])
+                print(f"{n}. Inflacja została zbadana dnia {data_badania_inflacji} i wynosi {inflacja}%.")
+                if liczba_dni < 730:
+                    current_value = current_value + current_value * \
+                        (liczba_dni - 365)/365 * (inflacja + marza_kolejne_lata)/ 100
+                    print(f"Oprocentowanie w tym okresie wynosi {inflacja + marza_kolejne_lata}%.")
+                    print(f"Wartosc po tym okresie wynosi {round(current_value,2)} zł.\n")
+                else:
+                    current_value = current_value + current_value * \
+                        (inflacja + marza_kolejne_lata) / 100
+                    liczba_dni = liczba_dni - 365
+                    print(f"Oprocentowanie w tym okresie wynosi {inflacja + marza_kolejne_lata}%.")
+                    print(f"Wartosc po tym okresie wynosi {round(current_value,2)} zł.\n")
+                n = n + 1 
+                
+        print(f"Wartosc w dniu obecnym dla tickera {ticker} wynosi {round(current_value, 2)} zł.")
+        result_df = pd.concat([result_df, \
+                               pd.DataFrame(data=[[ticker, data_zakupu, \
+                                                   round(current_value, 2)]], \
+                                            columns=['Ticker', 'Date', 'Current Value'])])
+        result_dfs = result_df.merge(right=dane_obligacji, 
+                        how='inner',
+                        left_on=['Ticker', 'Date'],
+                        right_on= ['Ticker', 'Transaction_date'])
+        data_to_export = result_dfs
+        data_to_export['Date'] = current_date
+        data_to_export['Close'] = (data_to_export['Current Value']/\
+                                   data_to_export['Transaction_amount']).round(3)
+        data_to_export['Volume'] = 0
+        data_to_export['Turnover'] = 0
+        data_to_export = data_to_export[['Ticker', 'Date', 'Close', \
+                                         'Volume', 'Turnover']]
+            
+        schema = [bigquery.SchemaField(name = 'Ticker', field_type = "STRING", \
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Date', field_type = "DATE",\
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Close', field_type = "FLOAT",\
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Volume', field_type = "INTEGER",\
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Turnover', field_type = "INTEGER",\
+                                       mode = "NULLABLE")]
+            
+            # 1.5. Zdefiniowanie parametrów tabeli, do której pisze CF.
+        project_id = 'projekt-inwestycyjny'
+        dataset_id = 'Dane_instrumentow'
+        table_id = 'Daily'
+        destination_table = f"{project_id}.{dataset_id}.{table_id}"
+      
+        # 18. Wyznaczenie konfiguracji dla joba i wykonanie joba.
+        job_config = bigquery.LoadJobConfig(schema = schema,
+                                            write_disposition = "WRITE_APPEND")
+        try:
+            job = client.load_table_from_dataframe(data_to_export, 
+                                                   destination_table,
+                                                   job_config = job_config)
+            job.result()
+        except Exception as e:
+            print(f"Error uploading data to BigQuery: {str(e)}")
+            return "Błąd eksportu danych do BigQuery."
+        
+        print("Dane obligacji skarbowych zostały przekazane do tabeli BigQuery.")
+        return "Program zakończył się pomyślnie."
+        
+        
+"""
+Konfiguracja:
+    Region: europe-central2 
+    Typ aktywatora: Pub/Sub
+    Pamięć przydzielona: 256 MiB
+    CPU: 0.167
+    Przekroczony limit czasu: 60
+    Maksymalna liczba żądań na instancję: 1
+    Minimalna liczba instancji: 0
+    Maksymalna liczba instancji: 1
+    Konto usługi srodowiska wykonawczego:
+        Default Compute Service Account
+    
+Punkt wejscia:
+    obligacje_skarbowe
 
-# %%
 
-    saving_path = "D:\Inwestowanie,banki\Inwestowanie\Obligacje- analityka\Wyniki obligacji skarbowych.xlsx"
-    result_dfs.to_excel(excel_writer=saving_path, sheet_name='Obligacje skarbowe',
-                        columns=['Ticker', 'Ticker ID', 'Waluta', 'Data', \
-                                  'Wolumen', 'Wartość_bez_prowizji',\
-                                      'Current Value',\
-                                      'Kurs waluty transakcji'],
-                        index=False)
+Requirements:
+    functions-framework==3.*
+    datetime
+    pandas
+    pandas_gbq
+    google.cloud
+"""
