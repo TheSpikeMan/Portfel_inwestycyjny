a1e6e17289381758d44b607f474f3259640801d3
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-01-14 15:15:13 +0100

    Zmodyfikowano kod - usunięto zbędne printy i instrukcje

--- obligacje_skarbowe.py
+++ obligacje_skarbowe.py
@@ -64,15 +64,10 @@ def Treasury_bonds_daily(cloud_event):
     dane_marz = query_job_3.to_dataframe()
     
     dane_inflacyjne.columns = ['Inflacja', 'Początek miesiąca']
-    dane_inflacyjne['Inflacja'] = dane_inflacyjne['Inflacja'].astype('string')
-    dane_inflacyjne['Inflacja'] = dane_inflacyjne['Inflacja'].str.replace(".", ",")
-    
-    
     dane_obligacji = dane_transakcyjne.merge(right=dane_marz, 
                                      how='inner', 
                                      on = 'Ticker')
     
-        
         
     ticker_list = (dane_obligacji['Ticker'])
     dates_list = (dane_obligacji['Transaction_date'])
@@ -92,18 +87,12 @@ def Treasury_bonds_daily(cloud_event):
     result_df = pd.DataFrame(columns=['Ticker', 'Date', 'Current Value'])
     
     for dane in dane_do_analizy.iterrows():
-        #ticker = dane[1][0]
         ticker = dane[1].iloc[0]
-        #data_zakupu = dane[1][1]
         data_zakupu = dane[1].iloc[1]
-        #wolumen = dane[1][2]
         wolumen = dane[1].iloc[2]
         marza_pierwszy_rok = dane[1].iloc[3]
-        #marza_pierwszy_rok = dane[1][3]
         marza_kolejne_lata = dane[1].iloc[4]
-        #marza_kolejne_lata = dane[1][4]
         
-    
         # calculating the start_value
         wolumen_jednostkowy = 100
         start_value = wolumen * wolumen_jednostkowy
@@ -113,7 +102,6 @@ def Treasury_bonds_daily(cloud_event):
         liczba_dni = (current_date - data_zakupu).days
         liczba_lat = int(math.modf(liczba_dni/365)[1])
     
-            
         # writing the code to find current value
         n = 1
         if liczba_dni < 365:
@@ -121,33 +109,23 @@ def Treasury_bonds_daily(cloud_event):
         
         else:
             current_value = start_value + start_value * (marza_pierwszy_rok/100)
-            print(f"Obligacje zostały zakupione w dniu {data_zakupu} na wolumen {wolumen} sztuk.")
-            print(f"Marża w pierwszym roku wyniosła {marza_pierwszy_rok}%.")
-            print(f"Wartosc inwestycji, po pierwszym roku to {current_value} zł.\n")
             for i in range(liczba_lat, 0, -1):
                 liczba_dni_przesuniecie = timedelta(days= 365 * n - 60)
                 data_badania_inflacji = date((data_zakupu + liczba_dni_przesuniecie).year, \
                                      (data_zakupu + liczba_dni_przesuniecie).month, \
                                      1)
-                inflacja = dane_inflacyjne.loc[dane_inflacyjne['Początek miesiąca'] == str(data_badania_inflacji)]\
-                    .iloc[:,:1]
-                inflacja['Inflacja'] = inflacja['Inflacja'].str.replace(',','.')
-                inflacja = float(inflacja.iloc[0,0])
-                print(f"{n}. Inflacja została zbadana dnia {data_badania_inflacji} i wynosi {inflacja}%.")
+                inflacja = dane_inflacyjne.loc[dane_inflacyjne['Początek miesiąca'] \
+                                               == str(data_badania_inflacji)].iat[0,0]
+    
                 if liczba_dni < 730:
                     current_value = current_value + current_value * \
                         (liczba_dni - 365)/365 * (inflacja + marza_kolejne_lata)/ 100
-                    print(f"Oprocentowanie w tym okresie wynosi {inflacja + marza_kolejne_lata}%.")
-                    print(f"Wartosc po tym okresie wynosi {round(current_value,2)} zł.\n")
                 else:
                     current_value = current_value + current_value * \
                         (inflacja + marza_kolejne_lata) / 100
                     liczba_dni = liczba_dni - 365
-                    print(f"Oprocentowanie w tym okresie wynosi {inflacja + marza_kolejne_lata}%.")
-                    print(f"Wartosc po tym okresie wynosi {round(current_value,2)} zł.\n")
                 n = n + 1 
                 
-        print(f"Wartosc w dniu obecnym dla tickera {ticker} wynosi {round(current_value, 2)} zł.")
         result_df = pd.concat([result_df, \
                                pd.DataFrame(data=[[ticker, data_zakupu, \
                                                    round(current_value, 2)]], \
@@ -201,7 +179,7 @@ def Treasury_bonds_daily(cloud_event):
     print("Dane obligacji skarbowych zostały przekazane do tabeli BigQuery.")
     return "Program zakończył się pomyślnie."
 
-    
+
 """
 Konfiguracja:
 Region: europe-central2 
@@ -213,7 +191,7 @@ Maksymalna liczba żądań na instancję: 1
 Minimalna liczba instancji: 0
 Maksymalna liczba instancji: 1
 Konto usługi srodowiska wykonawczego:
-    Default Compute Service Account
+Default Compute Service Account
 
 Punkt wejscia:
 obligacje_skarbowe
