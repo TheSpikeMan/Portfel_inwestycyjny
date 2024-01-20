ca43e19029d454cdc4eadd4f14d6a2315b574b38
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-01-20 21:29:55 +0100

    Przepisano kod na OOP

--- markets_ft_webscraping.py
+++ markets_ft_webscraping.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 17 17:38:44 2024
-
-@author: grzeg
-"""
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -15,185 +8,195 @@ import functions_framework
 import pandas_gbq
 from flask import Flask, request
 
-
-def pobierz_aktualne_kursy_walut(project, dataset, table):
-    """
-
-    Parameters
-    ----------
-    project : STRING - projekt, z którego pobierane są aktualne kursy walut
-    dataset : STRING - dataset, z którego pobierane są aktualne kursy walut
-    table : STRING - tabela, z której pobierane są aktualne kursy walut
-
-    Returns
-    -------
-    present_currencies : DataFrame - tabela zawierająca informacje o aktualnych
-        kursach walut dla walut EUR oraz USD
-
-    """
+class ETFScraper():
     
-    # SQL pobiera aktualny kurs walut z Cloud Functions
-    client = bigquery.Client()
-    query = f"""
-    WITH
-    all_currencies_data_ordered AS (
-      SELECT
-        *,
-        ROW_NUMBER() OVER(PARTITION BY Currency ORDER BY Currency_date DESC) as row_number
-      FROM `{project}.{dataset}.{table}`
-      WHERE TRUE
-    )
-    
-    SELECT *
-    FROM all_currencies_data_ordered
-    WHERE row_number = 1
-            
-    """
-    
-    query_job = client.query(query = query)
-    present_currencies = query_job.to_dataframe()
-    return present_currencies
+    def __init__(self, 
+                 project, 
+                 dataset_currency, 
+                 dataset_instruments,
+                 table_currencies,
+                 table_instruments,
+                 table_instruments_types,
+                 table_daily):
+        """
 
-def pobierz_aktualne_instrumenty_ETF(project, dataset, table_1, table_2):
-    """
-    
-    Parameters
-    ----------
-    project : STRING - projekt, z którego pobierane są dane aktualnych instrumentów
-    dataset : STRING - dataset, zawierający dane aktualnych instrumentów
-    table_1 : STRING - tabela, zawierająca dane instrumentów
-    table_2 : STRING - tabela, zawierająca dane typów instrumentów
+        Parameters
+        ----------
+        project : STRING - projekt, z którego pobierane są aktualne kursy walut
+        dataset_currency : STRING - dataset, z którego pobierane są 
+            aktualne kursy walut
+        dataset_instruments : STRING - dataset, zawierający dane aktualnych 
+            instrumentów
+        table_currencies : STRING - tabela, z której pobierane są aktualne kursy walut
+        table_instruments : STRING - tabela, zawierająca dane instrumentów
+        table_instruments_types : STRING - tabela, zawierająca dane 
+            typów instrumentów
+        table_daily : STRING - tabela, zawierająca dane giełdowe instrumentów.
+            Jest to tabela docelowa dla eksportów.
 
-    Returns
-    -------
-    present_instruments : DataFrame - tabela zawierająca dane aktualnych instrumentów
-        ETF
+        Returns
+        -------
+        None.
 
-    """
-    
-    # SQL pobierający dane aktualnych instrumentow
-    query_2 = f"""
-    SELECT
-      Ticker,
-      Market,
-      Currency,
-      Instrument_type
-    FROM `{project}.{dataset}.{table_1}` AS inst
-    LEFT JOIN `{project}.{dataset}.{table_2}` AS inst_typ
-    ON inst.Instrument_type_id = inst_typ.Instrument_type_id
-    WHERE Status = 1
-    AND Instrument_type = 'ETF zagraniczne'
-    """
-    client = bigquery.Client()
-    query_job_2  = client.query(query = query_2)
-    present_instruments = query_job_2.to_dataframe()
-    return present_instruments
-
-def webscraping_ETFs(present_instruments, present_currencies):
-    """
-
-    Parameters
-    ----------
-    present_instruments : DataFrame - tabela zawierająca dane aktualnych instrumentów
-        ETF. Tabela pobierana jako wynik pracy funkcji 'pobierz_aktualne_instrumenty_ETF'
-    present_currencies : DataFrame - tabela zawierająca informacje o aktualnych
-        kursach walut dla walut EUR oraz USD. Tabela pobierana jako wynik pracy funkcji
-        pobierz_aktualne_kursy_walut
-    Returns
-    -------
-    data_to_export : DataFrame - dane gotowe do eksportu do BigQuery
-
-    """
-    
-    result_df = pd.DataFrame()
-    current_date = date.today()
-    for instrument in present_instruments.iterrows():
-
-        url = "https://markets.ft.com/data/etfs/tearsheet/summary?s=" + \
-                f"{instrument[1]['Ticker']}:" + \
-                f"{instrument[1]['Market']}:" + \
-                f"{instrument[1]['Currency']}"
+        """
+        self.project = project
+        self.dataset_currency = dataset_currency
+        self.dataset_instruments = dataset_instruments
+        self.table_currencies = table_currencies
+        self.table_instruments = table_instruments
+        self.table_instruments_types = table_instruments_types
+        self.table_daily = table_daily
+        
+        
+    def pobierz_aktualne_kursy_walut(self):
+        
+        print("Pobieram aktualne kursy walut.")
+        client = bigquery.Client()
+        query = f"""
+        WITH
+        all_currencies_data_ordered AS (
+          SELECT
+            *,
+            ROW_NUMBER() OVER(PARTITION BY Currency ORDER BY Currency_date DESC) as row_number
+          FROM `{self.project}.{self.dataset_currency}.{self.table_currencies}`
+          WHERE TRUE
+        )
+        
+        SELECT *
+        FROM all_currencies_data_ordered
+        WHERE row_number = 1
                 
-        with requests.get(url=url) as r:
-            if r.status_code == 200:
-                print('Connection to the site successful.')
-            
-            soup = BeautifulSoup(r.text, 'html.parser')
-            
-            close = soup.find_all('span', class_ = 'mod-ui-data-list__value')
-            close = float(close[0].text)
-            result_df = pd.concat([result_df, 
-                                   pd.DataFrame([[instrument[1]['Ticker'], close]])],
-                                   axis = 0)
-    result_df.columns = ['Ticker', 'Close']
-    data_to_export = present_instruments.merge(result_df,
-                                               how = 'inner',
-                                               on = 'Ticker')
-    data_to_export = data_to_export.merge(present_currencies,
-                                          how = 'inner',
-                                          on = 'Currency')
-    data_to_export['Close'] = (data_to_export['Close'] * \
-                               data_to_export['Currency_close']).\
-                               round(decimals = 2)
-    data_to_export['Date'] = current_date
-    data_to_export['Volume'] = 0 
-    data_to_export['Turnover'] = 0 
-
-    data_to_export = data_to_export.loc[:,['Ticker', 
-                                           'Date', 
-                                           'Close', 
-                                           'Turnover', 
-                                           'Volume']]
-    return data_to_export
-
-def eksport_danych_do_BigQuery(data_to_export, project, dataset, table):
-    """
-
-    Parameters
-    ----------
-    data_to_export : DataFrame - dane gotowe do eksportu do BigQuery. Pobrano z
-        funkcji webscraping_ETFs
-    project : STRING - projekt, do którego eksportowane są dane
-    dataset : STRING - dataset, do którego eksportowane są dane
-    table : STRING - tabela, do której eksportowane są dane
-
-    Returns
-    -------
-    None.
-
-    """
+        """
+        
+        query_job = client.query(query = query)
+        return query_job.to_dataframe()
     
-    client = bigquery.Client()
-    destination_table = f"{project}.{dataset}.{table}"
+    def pobierz_aktualne_instrumenty_ETF(self):
+        
+        print("Pobieram aktualne instrumenty w ramach ETF.")
+        query_2 = f"""
+        SELECT
+          Ticker,
+          Market,
+          Currency,
+          Instrument_type
+        FROM `{self.project}.{self.dataset_instruments}.{self.table_instruments}` AS inst
+        LEFT JOIN `{self.project}.{self.dataset_instruments}.{self.table_instruments_types}` AS inst_typ
+        ON inst.Instrument_type_id = inst_typ.Instrument_type_id
+        WHERE Status = 1
+        AND Instrument_type = 'ETF zagraniczne'
+        """
+        client = bigquery.Client()
+        query_job_2  = client.query(query = query_2)
+        return query_job_2.to_dataframe()
+        
     
-    schema = [bigquery.SchemaField(name = 'Ticker', field_type = "STRING", \
-                                   mode = "REQUIRED"),
-              bigquery.SchemaField(name = 'Date', field_type = "DATE",\
-                                   mode = "REQUIRED"),
-              bigquery.SchemaField(name = 'Close', field_type = "FLOAT",\
-                                   mode = "REQUIRED"),
-              bigquery.SchemaField(name = 'Volume', field_type = "INTEGER",\
-                                   mode = "REQUIRED"),
-              bigquery.SchemaField(name = 'Turnover', field_type = "INTEGER",\
-                                   mode = "NULLABLE")]
-    
-    job_config = bigquery.LoadJobConfig(schema = schema,
-                                        write_disposition = "WRITE_APPEND")
+    def webscraping_ETFs(self,
+                         present_instruments,
+                         present_currencies):
+        
+        print("Dokonuję webscrapingu dla wybranych instrumentów ETF.")
+        result_df = pd.DataFrame()
+        current_date = date.today()
+        for instrument in present_instruments.iterrows():
 
-    # Wykonanie operacji eksportu danych.
-    try:
-        job = client.load_table_from_dataframe(data_to_export, 
-                                               destination_table,
-                                               job_config = job_config)
-        job.result()
-    except Exception as e:
-        print(f"Error uploading data to BigQuery: {str(e)}")
+            url = "https://markets.ft.com/data/etfs/tearsheet/summary?s=" + \
+                    f"{instrument[1]['Ticker']}:" + \
+                    f"{instrument[1]['Market']}:" + \
+                    f"{instrument[1]['Currency']}"
+                    
+            with requests.get(url=url) as r:
+                if r.status_code == 200:
+                    print(f"Podłączono się do strony - {instrument[1]['Ticker']}.")
+                
+                soup = BeautifulSoup(r.text, 'html.parser')
+                
+                close = soup.find_all('span', class_ = 'mod-ui-data-list__value')
+                close = float(close[0].text)
+                result_df = pd.concat([result_df, 
+                                       pd.DataFrame([[instrument[1]['Ticker'], close]])],
+                                       axis = 0)
+        result_df.columns = ['Ticker', 'Close']
+        data_to_export = present_instruments.merge(result_df,
+                                                   how = 'inner',
+                                                   on = 'Ticker')
+        data_to_export = data_to_export.merge(present_currencies,
+                                              how = 'inner',
+                                              on = 'Currency')
+        data_to_export['Close'] = (data_to_export['Close'] * \
+                                   data_to_export['Currency_close']).\
+                                   round(decimals = 2)
+        data_to_export['Date'] = current_date
+        data_to_export['Volume'] = 0 
+        data_to_export['Turnover'] = 0 
 
-    print("Dane zagranicznych ETF zostały przekazane do tabeli BigQuery.")
+        data_to_export = data_to_export.loc[:,['Ticker', 
+                                               'Date', 
+                                               'Close', 
+                                               'Turnover', 
+                                               'Volume']]
+        return data_to_export
+        
     
+    def run_scraper(self):
+        present_currencies = self.pobierz_aktualne_kursy_walut()
+        present_instruments = self.pobierz_aktualne_instrumenty_ETF()
+        data_to_export = self.webscraping_ETFs(present_instruments, present_currencies)
+        exporter = BigQueryExporter(self.project, 
+                                    self.dataset_instruments,
+                                    self.table_daily)
+        exporter.eksport_danych_do_BigQuery(data_to_export)
+        
+class BigQueryExporter():
+    
+    def __init__(self, project, dataset_to_export, table_to_export):
+        """
+
+        Parameters
+        ----------
+        project : STRING - projekt, do którego eksportowane są dane giełdowe
+        dataset_to_export : STRING - dataset, do którego eksportowane są dane giełdowe
+        table_to_export : STRING - tabela, do której eksportowane są dane giełdowe
+
+        Returns
+        -------
+        None.
+
+        """
+        self.project = project
+        self.dataset_to_export = dataset_to_export
+        self.table_to_export = table_to_export
+    
+    def eksport_danych_do_BigQuery(self, data_to_export):
+        print("Eksportuję dane do BigQuery.")
+        client = bigquery.Client()
+        destination_table = f"{self.project}.{self.dataset_to_export}.{self.table_to_export}"
+        
+        schema = [bigquery.SchemaField(name = 'Ticker', field_type = "STRING", \
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Date', field_type = "DATE",\
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Close', field_type = "FLOAT",\
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Volume', field_type = "INTEGER",\
+                                       mode = "REQUIRED"),
+                  bigquery.SchemaField(name = 'Turnover', field_type = "INTEGER",\
+                                       mode = "NULLABLE")]
+        
+        job_config = bigquery.LoadJobConfig(schema = schema,
+                                            write_disposition = "WRITE_APPEND")
+
+        try:
+            job = client.load_table_from_dataframe(data_to_export, 
+                                                   destination_table,
+                                                   job_config = job_config)
+            job.result()
+            print("Dane zagranicznych ETF zostały przekazane do tabeli BigQuery.")
+        except Exception as e:
+            print(f"Error uploading data to BigQuery: {str(e)}")
 
 @functions_framework.cloud_event
-def ETFs_daily(cloud_event):  
+def ETFs_daily(cloud_event): 
     """
 
     Parameters
@@ -208,27 +211,19 @@ def ETFs_daily(cloud_event):
     """
     
     project = 'projekt-inwestycyjny'
-    dataset = 'Waluty'
-    dataset_2 = 'Dane_instrumentow'
-    table = 'Currency'
-    table_1 = 'Instruments'
-    table_2 = 'Instrument_types'
-    table_3 = 'Daily'
+    dataset_currency = 'Waluty'
+    dataset_instruments = 'Dane_instrumentow'
+    table_currencies = 'Currency'
+    table_instruments = 'Instruments'
+    table_instruments_types = 'Instrument_types'
+    table_daily = 'Daily'
     
-    present_currencies = pobierz_aktualne_kursy_walut(project=project, 
-                                                       dataset=dataset, 
-                                                       table=table)
+    scraper = ETFScraper(project, 
+                        dataset_currency, 
+                        dataset_instruments, 
+                        table_currencies, 
+                        table_instruments,
+                        table_instruments_types, 
+                        table_daily)
     
-    present_instruments = pobierz_aktualne_instrumenty_ETF(project=project, 
-                                                           dataset=dataset_2, 
-                                                           table_1=table_1, 
-                                                           table_2=table_2)
-    
-    data_to_export = webscraping_ETFs(present_instruments=present_instruments,
-                                      present_currencies=present_currencies)
-    
-    
-    eksport_danych_do_BigQuery(data_to_export = data_to_export, 
-                               project=project, 
-                               dataset=dataset_2, 
-                               table=table_3)
+    scraper.run_scraper()
