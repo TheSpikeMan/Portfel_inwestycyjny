6ec6332d853f99cc2729e1342be472a2f5adac15
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-02-18 14:24:22 +0100

    Podpięto cały scraping do kodu. Do wprowadzenia komentarze, alternatywny scraping z yahoo i innych źródeł. Następnie przepicie na Cloud Functions.

--- webscraping_in_total.py
+++ webscraping_in_total.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 from datetime import date
 from google.cloud import bigquery
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import requests
+import math
 
 class Scraper():
     
@@ -12,19 +14,29 @@ class Scraper():
                  dataset_instruments,
                  dataset_currencies,
                  dataset_daily,
+                 dataset_inflation,
+                 dataset_transactions,
                  table_instruments,
                  table_instruments_types,
                  table_currencies,
-                 table_daily):
-        
+                 table_daily,
+                 table_inflation,
+                 table_treasury_bonds,
+                 view_transactions):
+    
         self.project_id = project_id
         self.dataset_instruments = dataset_instruments
         self.dataset_currencies = dataset_currencies
         self.dataset_daily = dataset_daily
+        self.dataset_inflation = dataset_inflation
+        self.dataset_transactions = dataset_transactions
         self.table_instruments = table_instruments
         self.table_instruments_types = table_instruments_types
         self.table_currencies = table_currencies
         self.table_daily = table_daily
+        self.table_inflation = table_inflation
+        self.table_treasury_bonds = table_treasury_bonds
+        self.view_transactions = view_transactions
 
     def pobierz_aktualne_instrumenty(self):
 
@@ -87,10 +99,128 @@ class Scraper():
             return query_job_3.to_dataframe()
         except:
             print("Podczas pobierania danych walutowych nastąpił błąd.")
+
+    def zbadaj_dane_inflacyjne(self):
+
+        destination_table_1 = f"`{project_id}.{dataset_inflation}.{table_inflation}`"
+        destination_table_2 = f"`{project_id}.{dataset_transactions}.{view_transactions}`"
+        destination_table_3 = f"`{project_id}.{dataset_instruments}.{table_treasury_bonds}`"
+
+        query_1 = f"""
+        SELECT
+            inflation,
+            date
+        FROM {destination_table_1}
+        WHERE TRUE
+        """
+
+        query_2 = f"""
+        SELECT
+            *
+        FROM  {destination_table_2}
+        WHERE
+            Instrument_type_id = 5
+        """
+        
+        query_3 = f"""
+        SELECT
+            *
+        FROM  {destination_table_3}
+        WHERE
+            TRUE
+        """
     
+        client = bigquery.Client()
+        query_job_1 = client.query(query_1)
+        query_job_2 = client.query(query_2)
+        query_job_3 = client.query(query_3)
+        dane_inflacyjne = query_job_1.to_dataframe()
+        dane_transakcyjne = query_job_2.to_dataframe()
+        dane_marz = query_job_3.to_dataframe()
+
+        return dane_inflacyjne, dane_transakcyjne, dane_marz
+
+    def obligacje_skarbowe(self,
+                           dane_inflacyjne, 
+                           dane_transakcyjne, 
+                           dane_marz):
+        
+        dane_inflacyjne.columns = ['Inflacja', 'Początek miesiąca']
+        dane_obligacji = dane_transakcyjne.merge(right=dane_marz, 
+                                        how='inner', 
+                                        on = 'Ticker')
+        dane_do_analizy = dane_obligacji.loc[:,['Ticker', 'Transaction_date',\
+                                                'Transaction_amount', \
+                                                'First_year_interest', \
+                                                'Regular_interest']]
+        result_df = pd.DataFrame(columns=['Ticker', 'Date', 'Current Value'])
+        for dane in dane_do_analizy.iterrows():
+            ticker = dane[1].iloc[0]
+            data_zakupu = dane[1].iloc[1]
+            wolumen = dane[1].iloc[2]
+            marza_pierwszy_rok = dane[1].iloc[3]
+            marza_kolejne_lata = dane[1].iloc[4]
+            
+            wolumen_jednostkowy = 100
+            
+            start_value = wolumen * wolumen_jednostkowy
+        
+            current_date = date.today()
+            liczba_dni = (current_date - data_zakupu).days
+            liczba_lat = int(math.modf(liczba_dni/365)[1])
+        
+            n = 1
+            
+            if liczba_dni < 365:
+                current_value = start_value + start_value * liczba_dni / 365 * (marza_pierwszy_rok/100)
+            
+            else:
+                current_value = start_value + start_value * (marza_pierwszy_rok/100)
+                
+                for i in range(liczba_lat, 0, -1):
+                    
+                    liczba_dni_przesuniecie = timedelta(days= 365 * n - 60)
+                    
+                    data_badania_inflacji = date((data_zakupu + liczba_dni_przesuniecie).year, \
+                                        (data_zakupu + liczba_dni_przesuniecie).month, \
+                                        1)
+                    inflacja = dane_inflacyjne.loc[dane_inflacyjne['Początek miesiąca'] \
+                                                == str(data_badania_inflacji)].iat[0,0]
+                    
+                    if liczba_dni < 730:
+                        current_value = current_value + current_value * \
+                            (liczba_dni - 365)/365 * (inflacja + marza_kolejne_lata)/ 100
+                    else:
+                        current_value = current_value + current_value * \
+                            (inflacja + marza_kolejne_lata) / 100
+                        liczba_dni = liczba_dni - 365
+                    n = n + 1 
+            
+            result_df = pd.concat([result_df, \
+                                pd.DataFrame(data=[[ticker, data_zakupu, \
+                                                    round(current_value, 2)]], \
+                                                columns=['Ticker', 'Date', 'Current Value'])])
+            data_to_export = result_df.merge(right=dane_obligacji, 
+                            how='inner',
+                            left_on=['Ticker', 'Date'],
+                            right_on= ['Ticker', 'Transaction_date'])
+    
+            data_to_export['Date'] = current_date
+            data_to_export['Close'] = (data_to_export['Current Value']/data_to_export['Transaction_amount'])
+
+            data_to_export_obligacje = data_to_export.groupby(['Ticker', 'Date']).\
+                apply(lambda x: np.average(x['Close'], \
+                weights=x['Transaction_amount']))\
+                .reset_index(name='Close').\
+                round(decimals = 3)
+            data_to_export_obligacje['Volume'] = 0
+            data_to_export_obligacje['Turnover'] = 0
+            
+        return data_to_export_obligacje
+  
     def webscraping_markets_ft_webscraping(self,
-                                           present_instruments_ETF,
-                                           present_currencies):
+                                        present_instruments_ETF,
+                                        present_currencies):
         
         print("Dokonuję webscrapingu dla wybranych instrumentów ETF.")
         result_df = pd.DataFrame()
@@ -111,28 +241,28 @@ class Scraper():
                 close = soup.find_all('span', class_ = 'mod-ui-data-list__value')
                 close = float(close[0].text)
                 result_df = pd.concat([result_df, 
-                                       pd.DataFrame([[instrument[1]['Ticker'], close]])],
-                                       axis = 0)
+                                    pd.DataFrame([[instrument[1]['Ticker'], close]])],
+                                    axis = 0)
                 
         result_df.columns = ['Ticker', 'Close']
         data_to_export = present_instruments_ETF.merge(result_df,
-                                                   how = 'inner',
-                                                   on = 'Ticker')
+                                                how = 'inner',
+                                                on = 'Ticker')
         data_to_export = data_to_export.merge(present_currencies,
-                                              how = 'inner',
-                                              on = 'Currency')
+                                            how = 'inner',
+                                            on = 'Currency')
         data_to_export['Close'] = (data_to_export['Close'] * \
-                                   data_to_export['Currency_close']).\
-                                   round(decimals = 2)
+                                data_to_export['Currency_close']).\
+                                round(decimals = 2)
         data_to_export['Date'] = current_date
         data_to_export['Volume'] = 0 
         data_to_export['Turnover'] = 0 
 
         data_to_export_ETFs = data_to_export.loc[:,['Ticker', 
-                                               'Date', 
-                                               'Close', 
-                                               'Turnover', 
-                                               'Volume']]
+                                            'Date', 
+                                            'Close', 
+                                            'Turnover', 
+                                            'Volume']]
         
 
         return data_to_export_ETFs
@@ -152,7 +282,7 @@ class Scraper():
                 current_date = date.today()
                 result_df = pd.DataFrame()
 
-                list_of_present_tickers = present_instruments_akcje.loc[:, 'Ticker']
+                list_of_present_tickers = list(present_instruments_akcje['Ticker'])
 
                 dict_of_tickers = {}
                 for index, tr_class in enumerate(trs_classes):
@@ -185,45 +315,53 @@ class Scraper():
                     result_df = pd.concat([result_df, pd.DataFrame([instruments])], axis = 0)
 
                 result_df.columns = ['Ticker' , 'Date', 'Close', 'Volume', 'Turnover']
+                print("Pobieranie danych z biznesradar zakończone powodzeniem.")
 
                 return result_df
             else:
 
-                print("Nie udało się podłączyć do strony.")
+                print("Nie udało się podłączyć do strony biznesradar.pl.")
         
     def run_scraper(self):
-        (present_instruments_ETF, present_instruments_akcje) = self.pobierz_aktualne_instrumenty()
+        present_instruments_ETF, present_instruments_akcje = self.pobierz_aktualne_instrumenty()
+        dane_inflacyjne, dane_transakcyjne, dane_marz = self.zbadaj_dane_inflacyjne()
         present_currencies = self.pobierz_aktualne_kursy_walut()
-        #data_to_export_ETFs = self.webscraping_markets_ft_webscraping(present_instruments_ETF,
-        #                                                              present_currencies)
+        data_to_export_ETFs = self.webscraping_markets_ft_webscraping(present_instruments_ETF,
+                                                                      present_currencies)
         data_to_export_akcje = self.webscraping_biznesradar(present_instruments_akcje,
                                                             present_currencies)
-        # data_to_export
-        #exporterObject = BigQueryExporter(
-        #    self.projekt_id,
-        #    self.dataset_instruments,
-        #    self.table_daily
-        #)
-        #exporterObject.exportDataToBigQuery(data_to_export_ETFs)
+        data_to_export_obligacje = self.obligacje_skarbowe(dane_inflacyjne,
+                                                           dane_transakcyjne,
+                                                           dane_marz)
+
+
+
+        data_to_export = pd.concat([data_to_export_ETFs, data_to_export_akcje, data_to_export_obligacje],
+                                   ignore_index = True)
+        exporterObject = BigQueryExporter(project_id,
+                                          dataset_instruments,
+                                          table_daily
+        )
+        exporterObject.exportDataToBigQuery(data_to_export)
 
 
 class BigQueryExporter():
 
     def __init__(self,
-                 projekt_to_export,
+                 project_to_export,
                  dataset_to_export,
                  table_to_export):
         
-        self.projekt_to_export = projekt_to_export
+        self.project_to_export = project_to_export
         self.dataset_to_export = dataset_to_export
         self.table_to_export = table_to_export
     
     def exportDataToBigQuery(self,
                              data_to_export):
         
-        print("Eksportuję dane do BigQuery.")
+        print("Eksportuję dane do BigQuery..")
         client = bigquery.Client()
-        destination_table = f"{self.project}.{self.dataset_to_export}.{self.table_to_export}"
+        destination_table = f"{self.project_to_export}.{self.dataset_to_export}.{self.table_to_export}"
         
         schema = [bigquery.SchemaField(name = 'Ticker', field_type = "STRING", \
                                        mode = "REQUIRED"),
@@ -244,28 +382,37 @@ class BigQueryExporter():
                                                    destination_table,
                                                    job_config = job_config)
             job.result()
-            print("Dane zostały przekazane do tabeli BigQuery.")
+            print("Dane zostały wyeksportowane do tabeli BigQuery.")
         except Exception as e:
             print(f"Error uploading data to BigQuery: {str(e)}")
-
 
 
 project_id = 'projekt-inwestycyjny'
 dataset_instruments = 'Dane_instrumentow'
 dataset_currencies = 'Waluty'
 dataset_daily = 'Dane_instrumentow'
+dataset_inflation = 'Inflation'
+dataset_transactions = 'Transactions'
 table_instruments = 'Instruments'
 table_instruments_types = 'Instrument_types'
 table_currencies = 'Currency'
 table_daily = 'Daily'
+table_inflation = 'Inflation'
+table_treasury_bonds = 'Treasury_Bonds'
+view_transactions = 'Transactions_view'
 
 scraper = Scraper(project_id, 
                   dataset_instruments,
                   dataset_currencies,
                   dataset_daily,
+                  dataset_inflation,
+                  dataset_transactions,
                   table_instruments, 
                   table_instruments_types,
                   table_currencies,
-                  table_daily)
+                  table_daily,
+                  table_inflation,
+                  table_treasury_bonds,
+                  view_transactions)
 
 result = scraper.run_scraper()
