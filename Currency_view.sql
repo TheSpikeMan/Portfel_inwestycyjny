818cb8c2d4e16888e37d3bd74dc5cae3257a6012
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-06-18 17:23:11 +0200

    Zmieniono nazwy CTE. Dodano kilka komentarzy.

--- Currency_view.sql
+++ Currency_view.sql
@@ -4,27 +4,29 @@ Wyznacza również ostatni kurs walutowy na potrzeby obliczeń transakcyjnych.
 */
 
 WITH
-Currency AS (SELECT * FROM `projekt-inwestycyjny.Waluty.Currency`),
-Dates AS (SELECT * FROM `projekt-inwestycyjny.Calendar.Dates`),
+currency    AS (SELECT * FROM `projekt-inwestycyjny.Waluty.Currency`),
+dates       AS (SELECT * FROM `projekt-inwestycyjny.Calendar.Dates`),
 
 /*
 Pobierz unikatowe transakcje z tabeli z walutami
 */
 
-Unique_currency  AS (
-    SELECT DISTINCT Currency.Currency FROM Currency
+unique_currency  AS (
+    SELECT DISTINCT 
+        currency.Currency 
+    FROM currency
 ),
 
 /*
 Połącz wszystkie dane kalendarzowe z danymi walut.
 */
 
-Dates_with_currency AS (
+dates_with_currency AS (
     SELECT
-        Dates.Date AS Currency_date,
+        dates.Date AS Currency_date,
         Currency   AS Currency
-    FROM Dates
-    CROSS JOIN Unique_currency
+    FROM dates
+    CROSS JOIN unique_currency
 ),
 
 /*
@@ -32,21 +34,21 @@ Utwórz okno oparte o pole waluty i podaj ostatnią nie nullową wartość dla d
 jest podawana. Jeżeli nie, szukana jest najbliższa nienullowa wartość.
 */
 
-Dates_with_non_null_currency AS (
+dates_with_non_null_currency AS (
     SELECT
         Dates_with_currency.Currency_date           AS Currency_date,
         Dates_with_currency.Currency                AS Currency,
         LAST_VALUE(Currency_close IGNORE NULLS) OVER last_currency_close AS Currency_close
-    FROM Dates_with_currency
-    LEFT JOIN Currency
-    ON Dates_with_currency.Currency_date = Currency.Currency_date
-    AND Dates_with_currency.Currency = Currency.Currency
+    FROM dates_with_currency
+    LEFT JOIN currency
+    ON dates_with_currency.Currency_date = currency.Currency_date
+    AND dates_with_currency.Currency = currency.Currency
     WINDOW
         last_currency_close AS (
             PARTITION BY Dates_with_currency.Currency ORDER BY Dates_with_currency.Currency_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
         )
     ORDER BY
-        Dates_with_currency.Currency_date
+        dates_with_currency.Currency_date
 )
 
 /*
@@ -54,12 +56,12 @@ Ostatni widok umożliwia wyciągnięcie ostatniej wartości wobec daty w danym w
 */
 
 SELECT
-    SAFE_CAST(Currency_date AS STRING)              AS Currency_date,
-    Currency                                        AS Currency,
-    Currency_close                                  AS Currency_close,
-    LAG(Currency_close) OVER last_currency_close    AS Last_day_currency
+    SAFE_CAST(Currency_date AS STRING)              AS Currency_date,    --> Potencjalna data transakcji
+    Currency                                        AS Currency,         --> Waluta
+    Currency_close                                  AS Currency_close,   --> Kurs zamknięcia na dany dzień
+    LAG(Currency_close) OVER last_currency_close    AS Last_day_currency --> Kurs zamknięcia na ostatni dzień roboczy
 FROM
-    Dates_with_non_null_currency
+    dates_with_non_null_currency
 WINDOW
     last_currency_close AS (
         PARTITION BY Currency ORDER BY Currency_date
