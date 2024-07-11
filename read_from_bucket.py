394a83a2b7702d5b9936de74188dad5f1a90d7e4
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-07-11 22:34:19 +0200

    Dodanie kodu do odczytu plików w ramach Google Cloud Storage

---
+++ read_from_bucket.py
@@ -0,0 +1,19 @@
+from google.cloud import storage
+
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    # bucket_name = "your-bucket-name"
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
+
+    # Note: The call returns a response only when the iterator is consumed.
+    for blob in blobs:
+        print(blob.name)
+
+
+# Listing names of blobs
+bucket_name = "portfel_inwestycyjny_desktop_app_bucket"
+list_blobs(bucket_name)
