3d49ffb23668d3a7a5c1a20055ac7de7b7e76a7d
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-07-26 22:12:17 +0200

    Kosmetyka

--- views/Dividend_view.sql
+++ views/Dividend_view.sql
@@ -5,7 +5,7 @@ Pobranie danych z widoku transakcyjnego.
 
 WITH
 Transactions_view AS (SELECT * FROM `projekt-inwestycyjny.Transactions.Transactions_view`),
-Daily_data AS (SELECT * FROM `projekt-inwestycyjny.Dane_instrumentow.Daily`),
+Daily_data        AS (SELECT * FROM `projekt-inwestycyjny.Dane_instrumentow.Daily`),
 
 -- PRELIMINARY AGGREGATION --
 /*
@@ -18,23 +18,26 @@ Oprócz tego dołożone jest połączenie danych dywidend z danymi danych giełd
 
 preliminary_aggregation AS (
 SELECT
-  Transactions_view.Ticker AS Ticker,
-  EXTRACT(YEAR FROM `Transaction_date`) AS year,
-  EXTRACT(MONTH FROM `Transaction_date`) AS month,
-  EXTRACT(DAY FROM `Transaction_date`) AS day,
-  Quarter AS quarter,
-  ROUND((Transaction_value_pln/Transaction_amount), 2) AS divident_price,
+  Transactions_view.Ticker                  AS Ticker,
+  EXTRACT(YEAR FROM `Transaction_date`)     AS year,
+  EXTRACT(MONTH FROM `Transaction_date`)    AS month,
+  EXTRACT(DAY FROM `Transaction_date`)      AS day,
+  Quarter                                   AS quarter,
+  ROUND((Transaction_value_pln/Transaction_amount), 2) 
+                                            AS divident_price,
   Transaction_value_pln AS  Transaction_value_pln,
-  MAX(Unit) AS unit,
-  MAX(Daily_data.Close) AS close,
-  SUM(SUM(Transaction_value_pln)) OVER(PARTITION BY Transactions_view.Ticker) AS divident_sum_total_per_ticker,
-  AVG(AVG(Transaction_value_pln)) OVER(PARTITION BY Transactions_view.Ticker) AS divident_average_total_per_ticker
+  MAX(Unit)                                 AS unit,
+  MAX(Daily_data.Close)                     AS close,
+  SUM(SUM(Transaction_value_pln)) OVER(PARTITION BY Transactions_view.Ticker) 
+                                            AS divident_sum_total_per_ticker,
+  AVG(AVG(Transaction_value_pln)) OVER(PARTITION BY Transactions_view.Ticker) 
+                                            AS divident_average_total_per_ticker
 FROM Transactions_view
 LEFT JOIN Daily_data
 ON Daily_data.Date = Transactions_view.Transaction_date 
 AND Transactions_view.Ticker = Daily_data.Ticker
-WHERE
-  Transaction_type_group = "Div_related_amount"
+WHERE TRUE
+  AND Transaction_type_group = "Div_related_amount"
 GROUP BY
   1,2,3,4,5,6,7
 ),
@@ -51,12 +54,11 @@ Dodatkowo wyznaczane są następujące wskaźniki:
 initital_aggregation AS (
 SELECT
   *,
-  SUM(Transaction_value_pln) OVER(PARTITION BY Ticker, year) AS divident_sum_per_ticker_and_year,
-  SUM(Transaction_value_pln) OVER(PARTITION BY year) AS divident_sum_per_year,
-  SUM(Transaction_value_pln) OVER(PARTITION BY year, quarter) AS divident_sum_per_year_and_quarter,
-  ROUND(100 * SAFE_DIVIDE(divident_price, close * unit), 2) AS dividend_ratio_pct
-FROM
-preliminary_aggregation 
+  SUM(Transaction_value_pln) OVER(PARTITION BY Ticker, year)    AS divident_sum_per_ticker_and_year,
+  SUM(Transaction_value_pln) OVER(PARTITION BY year)            AS divident_sum_per_year,
+  SUM(Transaction_value_pln) OVER(PARTITION BY year, quarter)   AS divident_sum_per_year_and_quarter,
+  ROUND(100 * SAFE_DIVIDE(divident_price, close * unit), 2)     AS dividend_ratio_pct
+FROM preliminary_aggregation 
 ),
 
 -- MID AGGREGATION VIEW --
@@ -68,22 +70,24 @@ Wyznaczona jest również wartość średniego divident ratio dla danego tickera
 */
 
 
-mid_aggregation AS(
+mid_aggregation AS (
 SELECT
-  Ticker,
-  year,
-  month,
-  day,
-  quarter,
-  ROUND(Transaction_value_pln,2) AS dividend_value_pln,
-  ROUND(divident_sum_total_per_ticker,2) AS divident_sum_total_per_ticker,
-  ROUND(divident_sum_per_ticker_and_year,2) AS divident_sum_per_ticker_and_year,
-  ROUND(divident_average_total_per_ticker,2) AS divident_average_total_per_ticker,
-  ROUND(divident_sum_per_year,2) AS divident_sum_per_year,
-  ROUND(divident_sum_per_year_and_quarter,2) AS divident_sum_per_year_and_quarter,
-  IFNULL(LAG(ROUND(divident_sum_per_ticker_and_year,2)) OVER(PARTITION BY Ticker ORDER BY year ),0) AS dividend_value_ticker_last_year,
-  IFNULL(dividend_ratio_pct, 0) AS dividend_ratio_pct,
-  IFNULL(ROUND(AVG(dividend_ratio_pct) OVER(PARTITION BY Ticker),2) , 0) AS avg_dividend_ratio_per_ticker_pct
+  Ticker                                        AS Ticker,
+  year                                          AS year,
+  month                                         AS month,
+  day                                           AS day,
+  quarter                                       AS quarter,
+  ROUND(Transaction_value_pln,2)                AS dividend_value_pln,
+  ROUND(divident_sum_total_per_ticker,2)        AS divident_sum_total_per_ticker,
+  ROUND(divident_sum_per_ticker_and_year,2)     AS divident_sum_per_ticker_and_year,
+  ROUND(divident_average_total_per_ticker,2)    AS divident_average_total_per_ticker,
+  ROUND(divident_sum_per_year,2)                AS divident_sum_per_year,
+  ROUND(divident_sum_per_year_and_quarter,2)    AS divident_sum_per_year_and_quarter,
+  IFNULL(LAG(ROUND(divident_sum_per_ticker_and_year,2)) OVER(PARTITION BY Ticker ORDER BY year ),0) 
+                                                AS dividend_value_ticker_last_year,
+  IFNULL(dividend_ratio_pct, 0)                 AS dividend_ratio_pct,
+  IFNULL(ROUND(AVG(dividend_ratio_pct) OVER(PARTITION BY Ticker),2) , 0) 
+                                                AS avg_dividend_ratio_per_ticker_pct
 FROM initital_aggregation
 ),
 
@@ -96,7 +100,11 @@ Dane są posortowane rosnąco po instrumencie finansowym, następnie malejąco p
 final_aggregation AS (
 SELECT
   * EXCEPT(day, month),
-  CONCAT(CAST(IFNULL(ROUND((100* SAFE_DIVIDE(divident_sum_per_ticker_and_year, dividend_value_ticker_last_year) - 100),2),0) AS STRING), "%") AS dividend_value_change_per_ticker_and_year
+  CONCAT(
+    CAST(
+      IFNULL(
+        ROUND((100* SAFE_DIVIDE(divident_sum_per_ticker_and_year, dividend_value_ticker_last_year) - 100),2),0) 
+    AS STRING), "%") AS dividend_value_change_per_ticker_and_year
 FROM
   mid_aggregation
 ORDER BY
