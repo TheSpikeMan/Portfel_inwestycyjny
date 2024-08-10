988561b97969f64368acfdf8556e738e0067c338
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-08-10 19:03:17 +0200

    Dodano kod umożliwiający pobranie danych tekstowych z Google Cloud Storage i przetworzeniu ich do formatu STRING

--- utils/read_from_bucket.py
+++ utils/read_from_bucket.py
@@ -1,7 +1,12 @@
 from google.cloud import storage
 
 def list_blobs(bucket_name):
-    """Lists all the blobs in the bucket."""
+    """
+    Lists all the blobs (files) in the bucket
+
+    bucket_name: STRING - name of the bucket
+    
+    """
     # bucket_name = "your-bucket-name"
 
     storage_client = storage.Client()
@@ -14,6 +19,27 @@ def list_blobs(bucket_name):
         print(blob.name)
 
 
-# Listing names of blobs
-bucket_name = "portfel_inwestycyjny_desktop_app_bucket"
-list_blobs(bucket_name)
+def read_sql_from_bucket(bucket_name, user_project, file_name):
+    """
+    Read sql from specific filename from bucket
+
+    bucket_name:  STRING - name of the bucket
+    user_project: STRING - project name
+    file_name   : STRING - file name with extension
+
+    """
+    storage_client = storage.Client()
+
+    # Creating a Bucket Class Object
+    bucket = storage_client.bucket(bucket_name=bucket_name, user_project=user_project)
+
+    # Reading a specific filename from bucket
+    blob_object = bucket.blob(blob_name=file_name)
+
+    # Downloading a blob object as 'bytes' object
+    blob_as_bytes = blob_object.download_as_bytes()
+
+    # Decoding the 'bytes' object to string
+    blob = blob_as_bytes.decode('UTF-8')
+
+    return blob
