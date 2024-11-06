c1ce0b07c74947c71270255d1fbce34164cbff86
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-11-06 22:50:01 +0100

    Dodanie iteracji po testowych danych, z parametryzacją.

---
+++ utils/Iterating_over_records_with_execute_immediate.sql
@@ -0,0 +1,27 @@
+BEGIN
+  CREATE TEMPORARY TABLE testing_set AS
+  WITH test_data AS (
+      SELECT 1 AS report_number, 'Szczecin' AS miasto
+      UNION ALL
+      SELECT 2 AS report_number, 'Poznań' AS miasto
+      UNION ALL
+      SELECT 3 AS report_number, 'Wrocław' AS miasto
+      UNION ALL
+      SELECT 4 AS report_number, 'Gdańsk' AS miasto
+  )
+
+  SELECT * FROM test_data;
+
+  FOR number IN (
+    SELECT DISTINCT
+      report_number
+    FROM testing_set
+  ) DO
+    EXECUTE IMMEDIATE FORMAT("""
+      SELECT *
+      FROM testing_set
+      WHERE report_number = %d
+      """, number.report_number
+    );
+  END FOR;
+END;
