a19b92a241867e32974fb040b3c4f3adc8ac5f90
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-02-08 20:35:32 +0100

    Dodanie tabeli 'transactions'

---
+++ tables/transactions.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `projekt-inwestycyjny.Transactions.Transactions`
+(
+  Transaction_id     INT64   OPTIONS(description="ID transakcji - indeks"),
+  Transaction_date   DATE    OPTIONS(description="Data transakcji"),
+  Transaction_type   STRING  OPTIONS(description="Typ transakcji"),
+  Currency           STRING  OPTIONS(description="Waluta"),
+  Transaction_price  FLOAT64 OPTIONS(description="Cena transakcyjna"),
+  Transaction_amount FLOAT64 OPTIONS(description="Liczba"),
+  Instrument_id      INT64   OPTIONS(description="ID instrumentu finansowego"),
+  Commision_id       FLOAT64 OPTIONS(description="Prowizja"),
+  Dirty_bond_price   FLOAT64 OPTIONS(description="Cena brudna"),
+  Tax_paid           BOOL    OPTIONS(description="Podatek zapłacony"),
+  Tax_value          FLOAT64 OPTIONS(description="Wartość podatku")
+);
