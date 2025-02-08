884d27f291d5c4c209dff301c2d0a420a2746b21
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-02-08 20:32:51 +0100

    Dodanie tabeli 'treasury_bonds'

---
+++ tables/treasury_bonds.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `projekt-inwestycyjny.Dane_instrumentow.Treasury_Bonds`
+(
+  Ticker              STRING  NOT NULL,
+  First_year_interest FLOAT64 NOT NULL,
+  Regular_interest    FLOAT64 NOT NULL
+);
