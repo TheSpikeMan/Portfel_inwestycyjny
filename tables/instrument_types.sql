6153559406fd1a957d0668e417523787916973a7
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-02-08 20:28:29 +0100

    Dodanie tabeli 'instrument_types'

---
+++ tables/instrument_types.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `projekt-inwestycyjny.Dane_instrumentow.Instrument_types`
+(
+  Instrument_type_id INT64  OPTIONS(description="ID typu instrumentu finansowego - indeks"),
+  Instrument_type    STRING OPTIONS(description="Nazwa typu instrumentu finansowego")
+);
