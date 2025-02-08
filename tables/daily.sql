255e21d513163a48aa205e2dcc1e424e8b5e7863
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-02-08 20:24:45 +0100

    Dodanie DDL dla tabeli 'daily'

---
+++ tables/daily.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `projekt-inwestycyjny.Dane_instrumentow.Daily`
+(
+  Ticker STRING(10)     NOT NULL OPTIONS(description="Ticker instrumentu finansowego"),
+  Date  DATE NOT        NULL     OPTIONS(description="Data aktualizacji danych"),
+  Close FLOAT64         NOT NULL OPTIONS(description="Kurs zamknięcia"),
+  Volume INT64          NOT NULL OPTIONS(description="Dzienny wolumen"),
+  Turnover INT64                 OPTIONS(description=""Dzienny obrót")
+);
