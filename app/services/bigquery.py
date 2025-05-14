6d405380eb2db3f80b2f142b74843c708ec3d656
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-05-14 18:40:25 +0200

    Aktualizacja pliku 'bigquery.py'

--- app/services/bigquery.py
+++ app/services/bigquery.py
@@ -17,33 +17,49 @@ DATASET_INSTRUMENTS = os.getenv("BQ_DATASET_INSTRUMENTS")
 TABLE_DAILY = os.getenv("BQ_TABLE_DAILY")
 
 # Pobierz ceny (np. 100 ostatnich rekordów) dla danego instrumentu
-def get_latest_prices_by_ticker(limit=100):
+async def get_latest_prices_by_ticker(ticker: str, limit: int = 30):
     query = f"""
         SELECT 
             Date, 
             Close
         FROM `{PROJECT_ID}.{DATASET_INSTRUMENTS}.{TABLE_DAILY}`
         WHERE TRUE
-            AND AND ticker = @ticker
-        ORDER BY date DESC
+            AND AND Ticker = @ticker
+        ORDER BY `Date` DESC
         LIMIT {limit}
     """
-    query_job = client.query(query)
-    return [dict(row) for row in query_job]
+
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[bigquery.ScalarQueryParameter("ticker", "STRING", ticker)]
+    )
+
+    try:
+        query_job = client.query(query, job_config=job_config)
+        results = query_job.result()
+        
+        return [{"date": row["Date"], "price": row["Close"]} for row in results]
+    
+    except Exception as e:
+        raise Exception(f"Error while fetching data: {str(e)}")
 
 # Pobierz ostatni dostępny kurs dla danego instrumentu
-def get_last_price_by_ticker(ticker: str):
+async def get_last_price_by_ticker(ticker: str):
+
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[bigquery.ScalarQueryParameter("ticker", "STRING", ticker)]
+    )
+
     query = f"""
-        SELECT close
+        SELECT Close
         FROM `{PROJECT_ID}.{DATASET_INSTRUMENTS}.{TABLE_DAILY}
         WHERE TRUE
-            AND ticker = @ticker
+            AND Ticker = @ticker
         QUALIFY TRUE
             AND ROW_NUMBER() OVER ticker_window = 1
         WINDOW
             ticker_window AS (
                 PARTITION BY
-                    ticker
+                    Ticker
                 ORDER BY
                     `Date` DESC
                 )
@@ -53,6 +69,16 @@ def get_last_price_by_ticker(ticker: str):
             bigquery.ScalarQueryParameter("ticker", "STRING", ticker)
         ]
     )
-    query_job = client.query(query, job_config=job_config)
-    rows = list(query_job)
-    return dict(rows[0]) if rows else None
+
+    try:
+        query_job = client.query(query, job_config=job_config)
+        results = query_job.result()
+        
+        if results.total_rows == 0:
+            return None  # Brak wyników
+        
+        row = list(results)[0]
+        return row["Close"]
+
+    except Exception as e:
+        raise Exception(f"Error while fetching data: {str(e)}")
