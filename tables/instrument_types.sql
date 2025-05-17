709026efc43bb428efe01cfaa5ce453260652759
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-05-17 13:51:58 +0200

    Poprawka 'DDL' tworzącego tabelę 'Instrument_types'

--- tables/instrument_types.sql
+++ tables/instrument_types.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `projekt-inwestycyjny.Dane_instrumentow.Instrument_types`
+CREATE OR REPLACE TABLE `projekt-inwestycyjny.Dane_instrumentow.Instrument_types`
 (
   Instrument_type_id INT64  OPTIONS(description="ID typu instrumentu finansowego - indeks"),
   Instrument_type    STRING OPTIONS(description="Nazwa typu instrumentu finansowego")
