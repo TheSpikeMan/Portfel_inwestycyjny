1a48ab7222e6c94ee0dad46f0a2a9b9515f30add
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-05-27 20:47:25 +0200

    Dodanie opisu do tabeli

--- tables/treasury_bonds.sql
+++ tables/treasury_bonds.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `projekt-inwestycyjny.Dane_instrumentow.Treasury_Bonds`
 (
-  Ticker              STRING  NOT NULL,
-  First_year_interest FLOAT64 NOT NULL,
-  Regular_interest    FLOAT64 NOT NULL
+  Ticker              STRING  NOT NULL OPTIONS(description="Ticker instrumentu inwestycyjnego"),
+  First_year_interest FLOAT64 NOT NULL OPTIONS(description="Marża w pierwszym roku"),
+  Regular_interest    FLOAT64 NOT NULL OPTIONS(description="Marża w kolejnych latach")
 );
