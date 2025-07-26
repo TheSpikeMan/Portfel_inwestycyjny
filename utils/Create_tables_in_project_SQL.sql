e8d6bec2681218f285a87aac416977e7714a0333
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-07-26 14:30:15 +0200

    Aktualizacja kodu DDLa tabel

--- utils/Create_tables_in_project_SQL.sql
+++ utils/Create_tables_in_project_SQL.sql
@@ -7,25 +7,51 @@ CREATE SCHEMA IF NOT EXISTS `projekt-inwestycyjny.Transactions`;
 CREATE SCHEMA IF NOT EXISTS `projekt-inwestycyjny.Waluty`;
 
 -- UTWORZENIE TABEL W ZBIORZE CALENDAR --
--- Tabela przechowująca przypisanie kwartału do określonej daty --
-CREATE TABLE IF NOT EXISTS `projekt-inwestycyjny.Calendar.Dates` (
-  `Date`  DATE      OPTIONS(description="Data"),
-  Quarter STRING    OPTIONS(description="Kwartał")
-);
+-- Tabela przechowuje zestaw dat od 2020-01-01 do 2030-12-31 wraz z innymi miarami takimi jak rok, miesiąć
+-- czy kwartał lub dzień roboczy
 
--- Uzupełnienie tabeli Dates --
-
-SELECT
-  *,
-  CASE
-    WHEN EXTRACT(MONTH FROM `Date`) IN (1, 2, 3)    THEN 'I kwartał'
-    WHEN EXTRACT(MONTH FROM `Date`) IN (4, 5, 6)    THEN 'II kwartał'
-    WHEN EXTRACT(MONTH FROM `Date`) IN (7, 8, 9)    THEN 'III kwartał'
-    WHEN EXTRACT(MONTH FROM `Date`) IN (10, 11, 12) THEN 'IV kwartał'
-  END AS Quarter
-FROM
-UNNEST(
-  GENERATE_DATE_ARRAY("2020-01-01", "2030-01-01", INTERVAL 1 DAY)) as `Date`;
+CREATE OR REPLACE TABLE `projekt-inwestycyjny.Calendar.Dates`
+  (
+    date           DATE   OPTIONS(description="Pełna data w formacie YYYY-MM-DD"),
+    year           INT64  OPTIONS(description="Rok danej daty (np. 2025)"),
+    month          INT64  OPTIONS(description="Numer miesiąca (1 = styczeń, 12 = grudzień)"),
+    day            INT64  OPTIONS(description="Dzień miesiąca (1–31)"),
+    quarter        INT64  OPTIONS(description="Numer kwartału (1–4)"),
+    quarter_text   STRING OPTIONS(description="Nazwa kwartału po polsku, np. 'III kwartał'"),
+    year_quarter   STRING OPTIONS(description="Połączenie roku i kwartału, np. '2025 Q3'"),
+    week           INT64  OPTIONS(description="Numer tygodnia wg ISO 8601 (poniedziałek = 1)"),
+    weekday        INT64  OPTIONS(description="Dzień tygodnia jako liczba od 0 (niedziela) do 6 (sobota)"),
+    is_working_day INT64  OPTIONS(description="1 = dzień roboczy (poniedziałek–piątek), 0 = weekend")
+  ) 
+  AS
+  SELECT
+    dates                              AS date,
+    EXTRACT(YEAR FROM dates)           AS year,
+    EXTRACT(MONTH FROM dates)          AS month,
+    EXTRACT(DAY FROM dates)            AS day,
+    EXTRACT(QUARTER FROM dates)        AS quarter,
+    CASE
+      WHEN EXTRACT(QUARTER FROM dates) = 1 THEN "I kwartał"
+      WHEN EXTRACT(QUARTER FROM dates) = 2 THEN "II kwartał"
+      WHEN EXTRACT(QUARTER FROM dates) = 3 THEN "III kwartał"
+      WHEN EXTRACT(QUARTER FROM dates) = 4 THEN "IV kwartał"
+    END                                AS quarter_text,
+    CONCAT(
+      SAFE_CAST(EXTRACT(YEAR FROM dates) AS STRING),
+      " Q",
+      SAFE_CAST(EXTRACT(QUARTER FROM dates) AS STRING)
+    )                                  AS year_quarter,
+    EXTRACT(ISOWEEK FROM dates)        AS week,
+    EXTRACT(DAYOFWEEK FROM dates) - 1  AS weekday,
+    CASE
+      WHEN EXTRACT(DAYOFWEEK FROM dates) - 1
+      BETWEEN 1 AND 5
+      THEN 1
+      ELSE 0
+      END                              AS is_working_day
+  FROM UNNEST(GENERATE_DATE_ARRAY('2020-01-01', '2030-12-31', INTERVAL 1 DAY) ) AS dates
+  ORDER BY
+    dates DESC
 
 
 -- UTWORZENIE TABEL W ZBIORZE DANE_INSTRUMENTÓW -- 
