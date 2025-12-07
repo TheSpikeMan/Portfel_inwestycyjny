98e46f1fae9d55f3974337c84668ac5bcecdaf33
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-12-07 13:36:01 +0100

    Dopasowuję się do nowego formatu danych

--- webscraping/biznesradar_webscraping/make_request.py
+++ webscraping/biznesradar_webscraping/make_request.py
@@ -4,7 +4,7 @@ import random
 from transform_data import transform_data
 import pandas as pd
 
-def make_request(urls_list: list):
+def make_request(urls_list: list[dict]):
 
     all_dfs = []
     """ Creating request"""
@@ -13,19 +13,16 @@ def make_request(urls_list: list):
             print("Rozpoczynam pobieranie danych.")
             index = 1
             for list_item in urls_list:
-                for url, params_list in list_item.items():
-                    # Pobranie zmiennych ze słownika z parametryzacją
+                for url, params_dict in list_item.items():
                     print(f"Trwa pobieranie danych: {index}/{len(urls_list)}.")
                     index += 1
-                    path = next(iter(params_list)).get('path')
-                    report_name = next(iter(params_list)).get('report_name')
                     r = s.get(url=url)
                     wait_time = random.randint(5, 10)
                     time.sleep(wait_time)
                     r.raise_for_status()
                     if r.status_code == 200:
                         data = r.text
-                        df = transform_data(data, path, report_name)
+                        df = transform_data(data, params_dict)
                         all_dfs.append(df)
         df_total = pd.concat(all_dfs, ignore_index=True)
         print("Rozpoczynam generowanie zbiorczego raportu...")
