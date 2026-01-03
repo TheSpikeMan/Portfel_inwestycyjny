d788ee311e3b0c430058a8318a85f2a0ad8b873c
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-01-03 12:50:39 +0100

    Przebudowa pliku make_url.py

--- webscraping/biznesradar_webscraping/make_url.py
+++ webscraping/biznesradar_webscraping/make_url.py
@@ -3,43 +3,33 @@ import re
 from biznesradar_webscraping_dict import (market_dict, reports_dict, data_dict, period_dict)
 
 
-def make_url(
-        market: str = 'gpw',
-        report_type: str = 'Raporty_finansowe',
-        sub_report_type: str = 'Rachunek_zyskow_i_strat',
-        measure: str = 'Przychody_ze_sprzedazy',
-        period: str = 'Kwartalne'):
+def make_url(df: pd.DataFrame) -> list[dict]:
+    """
+    :param df: DataFrame with all mapping dictionary
+    :return: list of dicts
+    """
 
+    """ Creating a copy of DataFrame to work with"""
+    df_work = df.copy()
 
-    """ Creating url """
-    result_dict = {}
+    """ Creating path and url """
+    results_list = []
     base_url = 'https://www.biznesradar.pl/'
-    sub_report_type_url = [sub_report_type_instance.get(sub_report_type)
-                           for sub_report_type_instance
-                           in reports_dict.get(report_type)
-                           if sub_report_type_instance.get(sub_report_type)][0]
 
-    market_url = market_dict.get(market)
-    measure_url = [measure_instance.get(measure)
-                   for measure_instance
-                   in data_dict.get(sub_report_type)
-                   if measure_instance.get(measure)][0]
-    period_url = [period_instance.get(period)
-                  for period_instance
-                  in period_dict.get('Okres')
-                  if period_instance.get(period)][0]
+    df_work['path'] = df_work['raport_slug'] + '/' + df_work['market_slug'] + '/' + df_work['period_slug'] + '/' + \
+        df_work['report_detailed_slug']
+    df_work['url'] = base_url + df_work['path']
 
-    """ Creating path """
-    path = f"{sub_report_type_url}/{market_url},{period_url},{measure_url}"
+    for row in df_work.itertuples(index=False):
+        results_dict.append({
+            row.url: {
+                'path': row.path,
+                'market': row.market,
+                'report_group': row.report_group,
+                'report_type': row.report_type,
+                'report_detailed': row.report_detailed,
+                'period': row.period
+            }
+        })
 
-    """ Joining base URL with custom part """
-    url = urljoin(base_url, path)
-
-    """ Assigning data to dict"""
-    result_dict[url] = {'path': path,
-                        'market': market,
-                        'report_type': report_type,
-                        'sub_report_type': sub_report_type,
-                        'measure': measure,
-                        'period': period}
-    return result_dict
+    return results_list
