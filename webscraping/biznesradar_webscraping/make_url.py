b2a7758eac2a6bb4866288c1e24a099c945bd78e
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-01-03 16:38:38 +0100

    Zmieniam listę na słownik, nie potrzebuję listy

--- webscraping/biznesradar_webscraping/make_url.py
+++ webscraping/biznesradar_webscraping/make_url.py
@@ -1,9 +1,10 @@
 from urllib.parse import urljoin
 import re
 from biznesradar_webscraping_dict import (market_dict, reports_dict, data_dict, period_dict)
+import pandas as pd
 
 
-def make_url(df: pd.DataFrame) -> list[dict]:
+def make_url(df: pd.DataFrame) -> dict:
     """
     :param df: DataFrame with all mapping dictionary
     :return: list of dicts
@@ -13,23 +14,21 @@ def make_url(df: pd.DataFrame) -> list[dict]:
     df_work = df.copy()
 
     """ Creating path and url """
-    results_list = []
+    results_dict = {}
     base_url = 'https://www.biznesradar.pl/'
 
-    df_work['path'] = df_work['raport_slug'] + '/' + df_work['market_slug'] + '/' + df_work['period_slug'] + '/' + \
+    df_work['path'] = df_work['raport_slug'] + '/' + df_work['market_slug'] + ',' + df_work['period_slug'] + ',' + \
         df_work['report_detailed_slug']
     df_work['url'] = base_url + df_work['path']
 
     for row in df_work.itertuples(index=False):
-        results_dict.append({
-            row.url: {
+        results_dict[row.url] = {
                 'path': row.path,
                 'market': row.market,
                 'report_group': row.report_group,
-                'report_type': row.report_type,
+                'report': row.report,
                 'report_detailed': row.report_detailed,
                 'period': row.period
             }
-        })
 
-    return results_list
+    return results_dict
