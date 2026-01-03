82f434bce13c7523ac8a2f1d1a1142551baa31ed
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-01-03 17:29:26 +0100

    Dodanie możliwych do uzupełnienia restrykcji, w kontekście generowanych raportów

--- webscraping/biznesradar_webscraping/main.py
+++ webscraping/biznesradar_webscraping/main.py
@@ -6,8 +6,32 @@ import pandas as pd
 
 if __name__ == '__main__':
 
+    """ 
+    Definining restrictions in webscraping 
+    FIELDS TO FILL IF RESTRICTIONS ARE TO BE SET
+    """
+
+    """ f.e. 'gpw' """
+    market_restrictions = []
+    """ f.e. 'Wskazniki' """
+    report_group_restrictions = []
+    """ f.e. 'Rachunek_zyskow_i_strat' """
+    reports_restrictions = []
+    """ f.e. 'Przychody_ze_sprzedazy' """
+    report_detailed_restrictions = []
+    """ f.e. 'Kwartalne' """
+    period_restrictions = []
+
+    restrictions = {
+        'market_restrictions': market_restrictions,
+        'report_group_restrictions': report_group_restrictions,
+        'report_restrictions': reports_restrictions,
+        'report_detailed_restrictions': report_detailed_restrictions,
+        'period_restrictions': period_restrictions
+    }
+
     """ Creating urls to request """
-    url_dict_to_request = make_url()
+    url_dict_to_request = make_url(restrictions)
 
     """ Creating requests to website"""
     make_request(url_dict_to_request)
