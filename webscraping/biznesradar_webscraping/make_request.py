6027a96912b8ca688475ad3f6e195fa35a7257d6
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-01-03 16:38:08 +0100

    Zmieniam typ danych z listy słowników na słownik

--- webscraping/biznesradar_webscraping/make_request.py
+++ webscraping/biznesradar_webscraping/make_request.py
@@ -14,28 +14,27 @@ def make_request(urls_list: list[dict]):
     """ Creating request"""
 
     with requests.session() as s:
-        print("Rozpoczynam pobieranie danych.")
-        for list_item in urls_list:
-            for url, params_dict in list_item.items():
-                print(f"Trwa pobieranie danych: {index}/{len(urls_list)}.")
-                index += 1
+        logger.info("Rozpoczynam pobieranie danych.")
+        for url, params_dict in urls_dict.items():
+            logger.info(f"Trwa pobieranie danych: {index}/{len(urls_dict)}.")
+            index += 1
 
-                try:
-                    r = s.get(url=url)
-                    r.raise_for_status()
-                except requests.RequestException as e:
-                    logger.error(f"Request failed for {url}: {e}")
-                    continue
+            try:
+                r = s.get(url=url)
+                r.raise_for_status()
+            except requests.RequestException as e:
+                logger.error(f"Request failed for {url}: {e}")
+                continue
 
-                data = r.text
-                df = transform_data(data, params_dict)
-                all_dfs.append(df)
+            data = r.text
+            df = transform_data(data, params_dict)
+            all_dfs.append(df)
 
-                if len(all_dfs) >= BATCH_SIZE:
-                    df_batch = pd.concat(all_dfs, ignore_index=True)
-                    send_data_to_bigquery(df_batch)
-                    all_dfs.clear()
-                time.sleep(random.randint(5, 10))
+            if len(all_dfs) >= BATCH_SIZE:
+                df_batch = pd.concat(all_dfs, ignore_index=True)
+                send_data_to_bigquery(df_batch)
+                all_dfs.clear()
+            time.sleep(random.randint(5, 10))
 
     if all_dfs:
         df_batch = pd.concat(all_dfs, ignore_index=True)
