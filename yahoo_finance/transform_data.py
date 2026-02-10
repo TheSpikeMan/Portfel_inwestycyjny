ad4860fd0dc27da249acbe21c60c4f5ffcbebcc7
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-02-10 20:39:12 +0100

    Dodanie logowania o liczbie poprawnych i niepoprawnych rekordów. Dodanie usuwania nulli.

--- yahoo_finance/transform_data.py
+++ yahoo_finance/transform_data.py
@@ -20,6 +20,13 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     df_with_suffixes['ticker_RIC'] = df_with_suffixes['ticker'] + df_with_suffixes['suffix']
     df_with_suffixes.drop(labels=['ticker', 'market', 'suffix'], axis=1, inplace=True)
 
+    logging.info(f"Number of total tickers: {df_with_suffixes['ticker_RIC'].size}")
+    logging.info(f"Number of correct tickers: {df_with_suffixes['ticker_RIC'].count()}")
+    logging.info(f"Number of incorrect tickers: {df_with_suffixes['ticker_RIC'].isna().sum()}")
+
+    # -- Removing null values --
+    df_with_suffixes.dropna(axis=0, subset=['ticker_RIC'], inplace=True)
+
     logging.info("Data transformation finished.")
 
     return df_with_suffixes
