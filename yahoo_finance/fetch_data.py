e4bcf6dc9a3c88635d8d2b22b72ff8907b977a21
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-02-13 17:41:39 +0100

    Correcting comments in fetch_data.py

--- yahoo_finance/fetch_data.py
+++ yahoo_finance/fetch_data.py
@@ -1,17 +1,17 @@
 import yfinance as yf
 import pandas as pd
 
+
 def fetch_data_from_yahoo_finance(
         tickers_list_to_fetch: list = None,
         tickers_dates_conf: dict = None
 ) -> pd.DataFrame | None:
     """
     :param
-        tickers_list_to_fetch: list of ticker to fetch from Yahoo Finance
-        tickers_start_date: starting date to fetch data
-        tickers_end_date: end date to fetch data
-        tickers_period: period to fetch data
+        tickers_list_to_fetch: list of tickers to fetch from Yahoo Finance
+        tickers_dates_conf: time-configurating dict
     :return:
+        result: DataFrame with data fetched
     """
 
     result = yf.download(
