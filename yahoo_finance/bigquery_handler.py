14175d45f2c0fd17a271eda34bf78adcf840fd36
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-02-13 19:09:38 +0100

    Added new function to load data to BigQuery

--- yahoo_finance/bigquery_handler.py
+++ yahoo_finance/bigquery_handler.py
@@ -35,3 +35,55 @@ def fetch_data_from_bigquery(sql: str, params: dict = None):
     except Exception as e:
         logging.error(f"Error while fetching data: {e}")
         return pd.DataFrame()
+
+
+def send_data_to_bigquery(df: pd.Dataframe, destination_params: dict = None):
+    """
+    :param
+        df: DataFrame to send to BigQuery table
+        destination_params: dictionary with destination parameters
+    :return: None
+    """
+
+    # -- DataFrame and dictionary validation part --
+    if df.empty:
+        logging.error("DataFrame is empty. Skipping export.")
+        return
+
+    missing_params = [name for name, value in destination_params.items() if value is None]
+    if missing_params:
+        raise ValueError("Destination parameters are missing: {', '.join(missing_params)} ")
+
+    # -- Basic transformations and declarations --
+    df.columns = [c.strip().replace(' ', '_').lower() for c in df.columns]
+
+    # -- Assigning parameters to variables --
+    project_name = destination_params.get('project_name')
+    dataset_name = destination_params.get('dataset_name')
+    table_name = destination_params.get('table_name')
+    location_name = destination_params.get('location_name')
+    destination_table = f"{project_name}.{dataset_name}.{table_name}"
+
+    # -- Starting sending data process --
+    client = bigquery.Client(project=destination_params['project_name'])
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND",
+        autodetect=True,
+        labels={
+            'module': 'webscraping',
+            'routine': 'ad-hoc'
+        },
+    )
+
+    # -- Sending data to BigQuery --
+    try:
+        load_job = client.load_table_from_dataframe(
+            dataframe=df,
+            destination=destination_table,
+            location=f"{location_name}",
+            job_config=job_config
+        )
+        load_job.result()
+    except Exception as e:
+        logging.error(f"Failed to load data to BigQuery: {e}")
+        raise
