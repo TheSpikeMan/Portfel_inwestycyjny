3b42a87f5b86846463745137c63de6cc847f2673
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-04-11 12:43:18 +0200

    [Present_transactions_view] Pozbycie się obliczeń procentowych w BigQuery. Usunięcie zaokrągleń.

--- views/Present_transactions_view.sql
+++ views/Present_transactions_view.sql
@@ -5,11 +5,11 @@ W widoku tym zawarte są wszystkie aktualne instrumenty, tzn. takie, które znaj
 */
 
 -- POBRANIE DANYCH --
-/* 
+/*
 W pierwszym kroku pobierane są dane transakcyjne, dane z giełdy oraz dane instrumentów, a także dane dywidendowe.
 */
 
-WITH 
+WITH
 transaction_view_raw      AS (SELECT * FROM `projekt-inwestycyjny.Transactions.Transactions_view`),
 daily                     AS (SELECT * FROM `projekt-inwestycyjny.Dane_instrumentow.Daily`),
 instruments               AS (SELECT * FROM `projekt-inwestycyjny.Dane_instrumentow.Instruments`),
@@ -62,7 +62,7 @@ amount_left_per_transaction AS (
       --> Jeżeli pozostała ilość jest mniejsza niż skumulowana ilość zakupów zwróć tymczasowo 0
       WHEN alpt.amount_left_per_ticker < SUM(transaction_amount) OVER sum_last_amount
       AND Transaction_type_group = "Buy_amount"
-      THEN 0 
+      THEN 0
       END                           AS amount_left
   FROM transaction_view             AS tvr
   LEFT JOIN amount_left_per_ticker  AS alpt
@@ -106,7 +106,7 @@ amount_left_per_transaction_corrected AS (
 corrected_again AS (
   SELECT
     *,
-    CASE 
+    CASE
       WHEN SUM(amount_left) OVER sum_last_amount <= amount_left_per_ticker
       THEN amount_left
       ELSE 0
@@ -141,25 +141,25 @@ present_instruments_view AS (
     Ticker                                                      AS Ticker,
     Name                                                        AS Name,
     Currency                                                    AS currency_exposure,
-    SUM(transaction_amount_left)                                AS ticker_present_amount, 
+    SUM(transaction_amount_left)                                AS ticker_present_amount,
     MAX(age_of_instrument)                                      AS max_age_of_instrument,
     ROUND(
-      SUM(transaction_amount_left * transaction_price * last_currency_close), 
+      SUM(transaction_amount_left * transaction_price * last_currency_close),
       2)                                                        AS ticker_buy_value,
     ROUND(
       SUM(transaction_amount_left * transaction_price * last_currency_close)/
-      SUM(transaction_amount_left), 
+      SUM(transaction_amount_left),
       2)                                                        AS ticker_average_close,
     MIN(Transaction_date)                                       AS minimum_buy_date
   FROM corrected_again
   WHERE TRUE
-    AND transaction_amount_left <> 0 
+    AND transaction_amount_left <> 0
   GROUP BY ALL
 ),
 
 -- DAILY DATA --
 /*
-W tym kroku wyciągane są ostatnie dane z giełdy, dzięki którym możliwe jest wyznaczenie aktualnego kursu 
+W tym kroku wyciągane są ostatnie dane z giełdy, dzięki którym możliwe jest wyznaczenie aktualnego kursu
 dla danego instrumentu
 */
 
@@ -180,11 +180,11 @@ daily_data AS (
 
 --- DYWIDENDY ---
 
--- DIVIDEND SELECTION -- 
+-- DIVIDEND SELECTION --
 /*
-W bieżącym kroku dokonywana jest analiza wszystkich transakcji dywidendowych, dla których data 
-wypłaty dywidendy jest większa od daty zakupu danego tickera, do którego przynależy dywidenda. 
-Dodatkowo liczony jest wskaźnik stopy dywidendy (na podstawie zestawienia ceny zamknięcia 
+W bieżącym kroku dokonywana jest analiza wszystkich transakcji dywidendowych, dla których data
+wypłaty dywidendy jest większa od daty zakupu danego tickera, do którego przynależy dywidenda.
+Dodatkowo liczony jest wskaźnik stopy dywidendy (na podstawie zestawienia ceny zamknięcia
 instrumentu, na moment wypłaty dywidendy).
 */
 
@@ -195,11 +195,10 @@ dividend_selection AS (
     transaction_view.Ticker                               AS Ticker,
     COALESCE(transaction_view.Close, 0)                   AS Close,
     COALESCE(
-        ROUND(100 * 
-          SAFE_DIVIDE(Transaction_price * Currency_close , 
-                      transaction_view.Close * Unit), 
-            2),
-            0)                                            AS dividend_ratio_pct,
+      SAFE_DIVIDE(
+        Transaction_price * Currency_close,
+        transaction_view.Close * Unit),
+        0)                                                AS dividend_ratio_pct,
     present_instruments_view.minimum_buy_date             AS minimum_buy_date,
     SUM(Transaction_value_pln) OVER ticker_window         AS dividend_sum, -- do sprawdzenia
     COUNT(Transaction_id)      OVER ticker_year_window    AS dividend_frequency
@@ -215,12 +214,12 @@ dividend_selection AS (
     AND present_instruments_view.minimum_buy_date < transaction_view.Transaction_date
   WINDOW
     ticker_window AS (
-      PARTITION BY 
+      PARTITION BY
         transaction_view.Project_id,
         transaction_view.Ticker
     ),
     ticker_year_window AS (
-      PARTITION BY 
+      PARTITION BY
         transaction_view.Project_id,
         transaction_view.Ticker,
         EXTRACT(YEAR FROM Transaction_date)
@@ -237,9 +236,9 @@ dividend_sum AS (
   SELECT
     dividend_selection.Project_id                   AS Project_id,
     dividend_selection.Ticker                       AS Ticker,
-    ROUND(SUM(Transaction_value_pln), 2)            AS dividend_sum,
-    ROUND(AVG(dividend_ratio_pct) * 
-          MAX(dividend_frequency), 2)               AS avg_dividend_ratio_per_ticker_pct
+    SUM(Transaction_value_pln)                      AS dividend_sum,
+    AVG(dividend_ratio_pct) *
+          MAX(dividend_frequency)                   AS avg_dividend_ratio_per_ticker_pct
   FROM dividend_selection
   GROUP BY ALL
   ),
@@ -276,66 +275,70 @@ present_instruments_plus_present_indicators AS (
     piv.ticker_present_amount                             AS ticker_present_amount,
     piv.ticker_average_close                              AS ticker_average_close,
     piv.ticker_buy_value                                  AS ticker_buy_value,
-    ROUND(
-        (piv.ticker_present_amount 
-        * daily.Close 
-        * inst.unit), 
-      2)                                                  AS ticker_present_value,
+    piv.ticker_present_amount * daily.Close * inst.unit   AS ticker_present_value,
     daily.Close * inst.unit                               AS current_price,
     daily.Date                                            AS current_price_date,
     piv.max_age_of_instrument                             AS max_age_of_instrument,
-    ROUND(100 * 
-        (piv.ticker_present_amount 
-        * daily.Close 
-        * inst.unit)/
-          SUM(piv.ticker_present_amount * daily.Close * inst.unit) OVER(PARTITION BY piv.Project_id),
-      2) 
-                                                          AS share_of_portfolio,
-    ROUND(100 * 
-        (piv.ticker_present_amount 
-        * daily.Close 
-        * inst.unit)/
-          SUM(piv.ticker_present_amount * daily.Close * inst.unit) 
-          OVER instrument_type_window, 
-      2) 
-                                                          AS share_of_portfolio_per_instrument_type,
-    ROUND(100 * 
-      ((piv.ticker_present_amount * daily.Close * inst.unit) /
-        piv.ticker_buy_value) - 100, 
-      2) 
-                                                          AS rate_of_return,
+    SAFE_DIVIDE(
+      piv.ticker_present_amount
+      * daily.Close
+      * inst.unit,
+      SUM(piv.ticker_present_amount * daily.Close * inst.unit)
+      OVER(PARTITION BY piv.Project_id))                  AS share_of_portfolio,
+    SAFE_DIVIDE(
+      piv.ticker_present_amount
+      * daily.Close
+      * inst.unit,
+      SUM(piv.ticker_present_amount * daily.Close * inst.unit)
+      OVER instrument_type_window
+      )                                                   AS share_of_portfolio_per_instrument_type,
+    SAFE_DIVIDE(
+      piv.ticker_present_amount * daily.Close * inst.unit,
+      piv.ticker_buy_value)
+      - 1                                                 AS rate_of_return,
     CASE
-      WHEN piv.max_age_of_instrument > 120 
-        THEN ROUND(
-              (365 * (100 * ((piv.ticker_present_amount * daily.Close * inst.unit)/piv.ticker_buy_value) - 100))
-              /piv.max_age_of_instrument, 2)
-    ELSE 0
-    END                                                   AS  yearly_rate_of_return,
+      WHEN piv.max_age_of_instrument > 120
+        THEN SAFE_DIVIDE(
+          365 * (SAFE_DIVIDE(
+            piv.ticker_present_amount * daily.Close * inst.unit,
+            piv.ticker_buy_value) - 1),
+            piv.max_age_of_instrument
+        )
+      ELSE 0
+      END                                                 AS yearly_rate_of_return,
     CASE
-      WHEN piv.max_age_of_instrument > 120 
-      THEN IFNULL(ROUND((365 * (100 * ((piv.ticker_present_amount * daily.Close * inst.unit + div_sum.dividend_sum)/piv.ticker_buy_value) - 100))
-      /piv.max_age_of_instrument, 2), ROUND((365 * (100 * ((piv.ticker_present_amount * daily.Close * inst.unit)/piv.ticker_buy_value) - 100))
-      /piv.max_age_of_instrument, 2))  
-    ELSE 0
-    END                                                   AS yearly_rate_of_return_incl_div,
-    ROUND((daily.Close * inst.unit - piv.ticker_average_close) * ticker_present_amount, 2) 
+      WHEN piv.max_age_of_instrument > 120
+      THEN COALESCE(
+        SAFE_DIVIDE(
+          365 * (SAFE_DIVIDE(
+            piv.ticker_present_amount * daily.Close * inst.unit + dividend_sum,
+            piv.ticker_buy_value) - 1),
+            piv.max_age_of_instrument
+        ),
+        365 * SAFE_DIVIDE(
+          SAFE_DIVIDE(piv.ticker_present_amount * daily.Close * inst.unit, piv.ticker_buy_value) - 1,
+          piv.max_age_of_instrument)
+      )
+      ELSE 0
+      END                                                 AS yearly_rate_of_return_incl_div,
+    ticker_present_amount * (daily.Close * inst.unit - piv.ticker_average_close)
                                                           AS profit,
-    IFNULL(ROUND(div_sum.dividend_sum + (daily.Close * inst.unit - piv.ticker_average_close) * piv.ticker_present_amount, 2),
-      ROUND((daily.Close * inst.unit - piv.ticker_average_close) * piv.ticker_present_amount, 2))  
-                                                          AS profit_incl_div,
-    IFNULL(div_sum.avg_dividend_ratio_per_ticker_pct, 0) 
-                                                          AS avg_dividend_ratio_per_ticker_pct
+  IFNULL(
+    div_sum.dividend_sum + (piv.ticker_present_amount * (daily.Close * inst.unit - piv.ticker_average_close)),
+    piv.ticker_present_amount * (daily.Close * inst.unit - piv.ticker_average_close)
+  )                                                       AS profit_incl_div,
+    IFNULL(div_sum.avg_dividend_ratio_per_ticker_pct, 0)  AS avg_dividend_ratio_per_ticker_pct
   FROM present_instruments_view AS piv
   LEFT JOIN Daily_data AS daily
-  ON piv.Ticker = daily.Ticker
+    ON piv.Ticker = daily.Ticker
   INNER JOIN instruments AS inst
-  ON piv.Project_id = inst.Project_id
-  AND piv.Ticker = inst.Ticker
+    ON piv.Project_id = inst.Project_id
+    AND piv.Ticker = inst.Ticker
   LEFT JOIN dividend_sum AS div_sum
-  ON piv.Project_id = div_sum.Project_id
-  AND piv.Ticker = div_sum.Ticker
+    ON piv.Project_id = div_sum.Project_id
+    AND piv.Ticker = div_sum.Ticker
   LEFT JOIN instrument_types AS inst_typ
-  ON inst.Instrument_type_id  = inst_typ.Instrument_type_id
+    ON inst.Instrument_type_id  = inst_typ.Instrument_type_id
   WINDOW
     instrument_type_window AS (
       PARTITION BY
@@ -347,12 +350,8 @@ present_instruments_plus_present_indicators AS (
 
 SELECT
   *,
-  ROUND(
-    (share_of_portfolio * yearly_rate_of_return_incl_div/100), 
-    2) AS yearly_rate_of_return_incl_div_weighted,
-  ROUND(
-    (share_of_portfolio_per_instrument_type * yearly_rate_of_return_incl_div/100), 
-    2) AS yearly_rate_of_return_incl_div_weighted_per_instrument_type                                             
+  share_of_portfolio * yearly_rate_of_return_incl_div                     AS yearly_rate_of_return_incl_div_weighted,
+  share_of_portfolio_per_instrument_type * yearly_rate_of_return_incl_div AS yearly_rate_of_return_incl_div_weighted_per_instrument_type
 FROM present_instruments_plus_present_indicators
 WHERE TRUE
 
