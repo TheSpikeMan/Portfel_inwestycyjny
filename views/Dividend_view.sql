1dba53afd1c6af2b90034067cbbac0345258145c
TheSpikeMan <grzegorz.noga.94@gmail.com>
2026-07-22 20:20:19 +0200

    [Dividend_view] Optymalizacja widoku

--- views/Dividend_view.sql
+++ views/Dividend_view.sql
@@ -4,33 +4,39 @@ Pobranie danych z widoku transakcyjnego.
 */
 
 WITH
-transactions_view AS (SELECT * FROM `projekt-inwestycyjny.Transactions.Transactions_view`),
-daily_data        AS (SELECT * FROM `projekt-inwestycyjny.Dane_instrumentow.Daily`),
-calendar          AS (SELECT * FROM `projekt-inwestycyjny.Calendar.Dates`),
+transactions_view_raw AS (SELECT * FROM `projekt-inwestycyjny.Transactions.Transactions_view`),
+daily_data            AS (SELECT * FROM `projekt-inwestycyjny.Dane_instrumentow.Daily`),
+calendar              AS (SELECT * FROM `projekt-inwestycyjny.Calendar.Dates`),
+
+-- FILTOWANIE I CASTOWANIE DANYCH --
+
+transactions_view AS (
+  SELECT
+    *,
+    EXTRACT(DATE FROM transaction_timestamp) AS transaction_date
+  FROM transactions_view_raw
+),
+
 
 -- PRELIMINARY AGGREGATION --
 /*
 W kroku tym wyciągany jest rok, miesiąc i dzień wypłaty dywidendy.
-Dodatkowo wyznaczana jest łączna suma wartości dywidendy per ticker i jest średnia wartość ze wszystkich wypłat.
+Dodatkowo wyznaczana jest łączna suma wartości dywidendy per ticker i jest średnia wartość ze wszystkich wypłat (dla całej dostępnej historii w projekcie)
 Oprócz tego dołożone jest połączenie danych dywidend z danymi danych giełdowych - dzięki czemu dokładane są kolumny:
-- dividend_price jako wartość dywidendy na jedną sztukę akcji w PLN,
-- MAX(Close) jako wartość akcji danego instrumentu na dzień wypłaty dywidendy - dzięki funkcji MAX nie trzeba tych danych agregować - i tak zawsze wychodzi jedna wartość, jako, że dane ściągane są jednokrotnie dla danego tickera w ciągu dnia
+- 'dividend_price' jako wartość dywidendy na jedną sztukę akcji w PLN,
+- 'close' jako wartość akcji danego instrumentu na dzień wypłaty dywidendy - dzięki funkcji MAX nie trzeba tych danych agregować - i tak zawsze wychodzi jedna wartość, jako, że dane ściągane są jednokrotnie dla danego tickera w ciągu dnia
 */
 
 preliminary_aggregation AS (
   SELECT
     tv.Project_id                             AS project_id,
     tv.Ticker                                 AS ticker,
-    calendar.year                             AS year,
-    calendar.month                            AS month,
-    calendar.day                              AS day,
-    calendar.quarter_text                     AS quarter,
-    SAFE_DIVIDE(
-      tv.Transaction_value_pln,
-      tv.Transaction_amount
-      )
-                                              AS dividend_price,
-    tv.Transaction_value_pln                  AS transaction_value_pln,
+    c.year                                    AS year,
+    c.month                                   AS month,
+    c.day                                     AS day,
+    c.quarter_text                            AS quarter,
+    tv.transaction_price                      AS dividend_price,
+    tv.transaction_value_pln                  AS transaction_value_pln,
     MAX(tv.Unit)                              AS unit,
     MAX(dd.Close)                             AS close,
     SUM(SUM(tv.Transaction_value_pln)) OVER ticker_in_project_window
@@ -39,13 +45,13 @@ preliminary_aggregation AS (
                                               AS dividend_average_total_per_ticker
   FROM transactions_view  AS tv
   LEFT JOIN daily_data    AS dd
-    ON dd.Date = CAST(tv.Transaction_timestamp AS DATE)
+    ON dd.Date = tv.transaction_date
     AND tv.Ticker = dd.Ticker
     AND
       (tv.Project_id = dd.Project_id
       OR dd.Project_id IS NULL)
-  LEFT JOIN calendar
-    ON CAST(tv.Transaction_timestamp AS DATE) = calendar.date
+  LEFT JOIN calendar AS c
+    ON tv.transaction_date = c.date
   WHERE TRUE
     AND Transaction_type_group = "Div_related_amount"
   GROUP BY ALL
@@ -63,7 +69,8 @@ W kroku tym wyciągane są wszystkie dane z 'preliminary aggregation'.
 Dodatkowo wyznaczane są następujące wskaźniki:
 - Skumulowana suma wartości dywidendy per ticker oraz year,
 - Skumulowana wartość dywidendy per year,
-- Skumulowana wartość dywidendy per year oraz quarter.
+- Skumulowana wartość dywidendy per year oraz quarter,
+- Wartość % dywidendy wobec kursu aktualnego na dzień wypłaty dywidendy
 */
 
 initital_aggregation AS (
@@ -78,8 +85,7 @@ initital_aggregation AS (
 
 -- MID AGGREGATION VIEW --
 /*
-W tej cześci w głównej mierze dokonywane jest zaokrąglenie wszystkich wskaźników do drugiego miejsca po przecinku.
-Dodatkowo wyznaczana jest wartość dywidendy dla danego instrumentu w całym roku wobec wartości dywidendy w poprzednim roku.
+W tej cześci wyznaczana jest wartość dywidendy dla danego instrumentu w całym roku wobec wartości dywidendy w poprzednim roku.
 Wykorzystywane są wszystkie dane z poprzednich kroków.
 Wyznaczona jest również wartość średniego dividend ratio dla danego tickera.
 */
@@ -93,26 +99,23 @@ mid_aggregation AS (
     month                                         AS month,
     day                                           AS day,
     quarter                                       AS quarter,
-    ROUND(transaction_value_pln,2)                AS dividend_value_pln,
-    ROUND(dividend_sum_total_per_ticker,2)        AS dividend_sum_total_per_ticker,
-    ROUND(dividend_sum_per_ticker_and_year,2)     AS dividend_sum_per_ticker_and_year,
-    ROUND(dividend_average_total_per_ticker,2)    AS dividend_average_total_per_ticker,
-    ROUND(dividend_sum_per_year,2)                AS dividend_sum_per_year,
-    ROUND(dividend_sum_per_year_and_quarter,2)    AS dividend_sum_per_year_and_quarter,
+    transaction_value_pln                         AS dividend_value_pln,
+    dividend_sum_total_per_ticker,
+    dividend_sum_per_ticker_and_year,
+    dividend_average_total_per_ticker,
+    dividend_sum_per_year,
+    dividend_sum_per_year_and_quarter,
     IFNULL(
       LAG(
-        ROUND(dividend_sum_per_ticker_and_year, 2)
+        dividend_sum_per_ticker_and_year
         )
         OVER ticker_in_project_window_ordered, 0)
                                                   AS dividend_value_ticker_last_year,
-    IFNULL(
-      ROUND(dividend_ratio_pct, 2)
-      , 0)                                        AS dividend_ratio_pct,
+    IFNULL(dividend_ratio_pct, 0)                 AS dividend_ratio_pct,
     IFNULL
     (
-      ROUND(
         AVG(dividend_ratio_pct)
-        OVER ticker_in_project_window, 2) , 0)
+        OVER ticker_in_project_window, 0)
                                                   AS avg_dividend_ratio_per_ticker_pct
   FROM initital_aggregation
   WINDOW
@@ -139,11 +142,9 @@ Dane są posortowane rosnąco po instrumencie finansowym, następnie malejąco p
 final_aggregation AS (
   SELECT
     * EXCEPT(day, month),
-    CONCAT(
-      CAST(
-        IFNULL(
-          ROUND((100* SAFE_DIVIDE(dividend_sum_per_ticker_and_year, dividend_value_ticker_last_year) - 100),2),0)
-      AS STRING), "%") AS dividend_value_change_per_ticker_and_year
+    IFNULL(
+      SAFE_DIVIDE(dividend_sum_per_ticker_and_year, dividend_value_ticker_last_year),
+      0) AS dividend_value_change_per_ticker_and_year
   FROM mid_aggregation
 )
 
@@ -154,14 +155,16 @@ SELECT
   ticker,
   year,
   quarter,
-  dividend_value_pln,
-  dividend_sum_total_per_ticker,
-  dividend_sum_per_ticker_and_year,
-  dividend_average_total_per_ticker,
-  dividend_sum_per_year,
-  dividend_sum_per_year_and_quarter,
-  dividend_value_ticker_last_year,
-  dividend_ratio_pct,
-  avg_dividend_ratio_per_ticker_pct,
-  dividend_value_change_per_ticker_and_year,
+  -- Wskaźniki prezentujące dane dla danego projektu i instrumentu --
+  dividend_ratio_pct,                   -- Wartość % dywidendy wobec kursu aktualnego dzień wypłaty dywidendy dla danego instrumentu w projekcie
+  avg_dividend_ratio_per_ticker_pct,    -- Średnia wartość % dywidendy wobec kursu aktualnego na dzień wypłaty dywidendy dla danego instrumentu w projekcie
+  dividend_value_change_per_ticker_and_year, -- Wartość % wzrostu lub spadku dywidendy dla danego instrumentu w projekcie
+  dividend_value_pln,                   -- Wartość dywidendy
+  dividend_sum_total_per_ticker,        -- Suma wartości wypłaconej dywidendy w całej historii dla danego instrumentu w projekcie
+  dividend_sum_per_ticker_and_year,     -- Suma wartości wypłaconej dywidendy w danym roku dla danego instrumentu w projekcie
+  dividend_average_total_per_ticker,    -- Średnia wartość wypłaconej dywidendy w całej historii dla danego instrumentu w projekcie
+  dividend_value_ticker_last_year,      -- Suma wartości wypłaconej dywidendy w poprzednim roku dla danego instrumentu w projekcie
+  -- Wskaźniki prezentujące dane dla danego projektu --
+  dividend_sum_per_year,                -- Suma wartości wypłaconej dywidendy w danym roku dla danego projektu
+  dividend_sum_per_year_and_quarter     -- Suma wartośći wypłaconej dywidendy w danym roku i kwartale dla danego projektu
 FROM final_aggregation
